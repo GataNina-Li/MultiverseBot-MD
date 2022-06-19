e2dd9df10f9cc93034bbd303f71b0e0e7308fbfb
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-19 00:05:56 -0500

    𝑬𝒔𝒕𝒆𝒍𝒂𝒓𝑩𝒐𝒕-𝑴𝑫 𝒑𝒐𝒓 𝑮𝒂𝒕𝒂 𝑫𝒊𝒐𝒔

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.pulsa = '085212023766'
 global.gopay = '087892711054'
 
 //=============『 Info Bot 』=================//
-global.namebot = 'EstelarBot-MD'
+global.namebot = '𝑬𝒔𝒕𝒆𝒍𝒂𝒓𝑩𝒐𝒕-𝑴𝑫 '
 global.gc = 'https://chat.whatsapp.com'
 global.web = 'https://youtube.com/channel/UCpNU4eY7eiI0ve05CssjdbA' //ubah jadi website lu, bisa link ig, link github, link yt, klo link gc ntr beda tampilan lagi. 
 global.price1 = '1 bulan = 10.000\n2 Minggu 7.000\n1 Minggu 5.000'
