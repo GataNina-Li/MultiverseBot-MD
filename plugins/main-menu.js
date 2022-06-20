90fea887e29df6c281014638460d1defd43295b2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-20 01:16:03 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -513,7 +513,7 @@ const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fro
           hydratedTemplate: {
             imageMessage: message.imageMessage, 
             hydratedContentText: text, 
-            hydratedFooterText: cmd, 
+            hydratedFooterText: `${cmd}`, 
             hydratedButtons: [{
             urlButton: {
                displayText: 'Website Creator',
