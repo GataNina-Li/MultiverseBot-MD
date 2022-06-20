a24eccfeec39a75dc223b65d428f5521d7909211
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-20 14:06:24 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -513,7 +513,7 @@ const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fro
           hydratedTemplate: {
             imageMessage: message.imageMessage, 
             hydratedContentText: text, 
-            hydratedFooterText: `Ⓛ ⇢ *_Limite_*\nⓅ ⇢ *_Premium_*`, 
+            hydratedFooterText: `Ⓛ ⇢ Limite\nⓅ ⇢ Premium\n𝑬𝒔𝒕𝒆𝒍𝒂𝒓𝑩𝒐𝒕-𝑴𝑫 | 𝑮𝒂𝒕𝒂 𝑫𝒊𝒐𝒔 `, 
             hydratedButtons: [{
             urlButton: {
                displayText: 'Website Creator',
