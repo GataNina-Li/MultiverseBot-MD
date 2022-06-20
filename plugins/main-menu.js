536b3f4e1cb579a487e580e8d1e7612db951c3c3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-20 01:09:40 -0500

    Update main-menu.js

--- plugins/main-menu.js
+++ plugins/main-menu.js
@@ -49,7 +49,7 @@ const defaultMenu = {
   header: '┌─「 %category 」',
   body: '├ %cmd %islimit %isPremium',
   footer: '└────\n', 
-  after: ``,
+  after: `yyyy`,
 }
 
 // ✩｡:*•.─────  AGRUPACION POR CATEGORIA  ─────.•*:｡✩
@@ -513,7 +513,7 @@ const template = generateWAMessageFromContent(m.key.remoteJid, proto.Message.fro
           hydratedTemplate: {
             imageMessage: message.imageMessage, 
             hydratedContentText: text, 
-            hydratedFooterText: cmd islimit isPremium`, 
+            hydratedFooterText: cmd + islimit + isPremium, 
             hydratedButtons: [{
             urlButton: {
                displayText: 'Website Creator',
