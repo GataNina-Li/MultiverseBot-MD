0b2aa4c837036de7680c50def78d7b0a2d613a8a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-22 18:42:46 -0500

    Update info-database.js

--- plugins/info-database.js
+++ plugins/info-database.js
@@ -1,3 +1,6 @@
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
+let fs = require('fs')
 let handler = async (m) => {
     let totalreg = Object.keys(global.db.data.users).length
     let rtotalreg = Object.values(global.db.data.users).filter(user => user.registered == true).length
