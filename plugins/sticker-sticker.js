8b98c2f43bb9ddc9b798e37ea3ae68c2cc201144
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-22 14:09:20 -0500

    𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫

--- plugins/sticker-sticker.js
+++ plugins/sticker-sticker.js
@@ -14,7 +14,7 @@ let handler = async (m, { conn }) => {
             if (!img) throw `reply image with command s`
             stiker = await sticker5(img, false, packname, author)
         } else if (/video/.test(mime)) {
-            if ((q.msg || q).seconds > 11) return m.reply('maksimal 10 detik!')
+            if ((q.msg || q).seconds > 11) return m.reply('máximo 10 segundos!')
             let img = await q.download()
             if (!img) throw `reply video with command s`
             stiker = await sticker5(img, false, packname, author)
