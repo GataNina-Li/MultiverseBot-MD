a7b9589e52ef512e8aa893d540322c07507a9510
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-22 16:09:48 -0500

    Update absen-delete.js

--- complementos/absen-delete.js
+++ complementos/absen-delete.js
@@ -1,4 +1,4 @@
-let handler = async (m, { usedPrefix }) => {
+/*let handler = async (m, { usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
     if (!(id in conn.absen)) conn.sendBut(m.chat, `_*No hay ausencias en este grupo.!*_\n\n*${usedPrefix}mulaiabsen* - empezar ausente`, wm, 'Empezar ausente', '.mulaiabsen', m)
@@ -10,4 +10,4 @@ handler.tags = ['absen']
 handler.command = /^(delete|hapus)absen$/i
 handler.group = true
 handler.admin = true
-module.exports = handler
+module.exports = handler*/
