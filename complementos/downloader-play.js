87ff196f2c96634bb1f0f6e248e3f0ca9c36aadb
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-22 14:31:48 -0600

    Update and rename plugins/downloader-play.js to complementos/downloader-play.js

--- complementos/downloader-play.js
+++ complementos/downloader-play.js
@@ -4,11 +4,11 @@ let fs = require('fs')
 let yts = require('yt-search')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text, usedPrefix }) => {
-  if (!text) throw `uhm.. cari apa?\n\ncontoh:\n${usedPrefix + command} california`
+  if (!text) throw `uhm.. que estas buscando?\n\npor ejemplo:\n${usedPrefix + command} california`
   let chat = global.db.data.chats[m.chat]
   let results = await yts(text)
   let vid = results.all.find(video => video.seconds < 3600)
-  if (!vid) throw 'Konten Tidak ditemukan'
+  if (!vid) throw 'Contenido no encontrado'
   let isVideo = /2$/.test(command)
   let yt = false
   let yt2 = false
