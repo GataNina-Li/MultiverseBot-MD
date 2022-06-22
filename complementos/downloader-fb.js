37ce887787ae5e8226ea2cc83d4d03045d588ca2
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-22 14:06:57 -0600

    Traducido por sofia

--- complementos/downloader-fb.js
+++ complementos/downloader-fb.js
@@ -1,9 +1,9 @@
 let xfar = require('xfarr-api')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text }) => {
-    if (!text) throw 'Masukkan Link\n\nContoh: .fb https://facebook.com/xxxxxx'
+    if (!text) throw 'Ingresar Link\n\nContoh: .fb https://facebook.com/xxxxxx'
   let res = await xfar.Facebook(text)
-m.reply('*Tunggu Sebentar...*')
+m.reply('*espera un rato...*')
 conn.sendFile(m.chat,res.medias[1].url, 'fb.mp4', `Tittle: ${res.tittle}
 Link Video: ${res.url}
 `, m)
