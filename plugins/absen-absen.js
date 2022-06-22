c346500a546453ce35a623a28abf3245180e8fc8
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-22 12:33:00 -0600

    Update absen-absen.js

--- plugins/absen-absen.js
+++ plugins/absen-absen.js
@@ -2,13 +2,13 @@ let handler = async (m, { conn, usedPrefix }) => {
     let id = m.chat
     conn.absen = conn.absen ? conn.absen : {}
     if (!(id in conn.absen)) {
-        await conn.sendBut(m.chat, `Tidak ada absen berlangsung!`, wm, 'Mulai', `${usedPrefix}mulaiabsen`, m)
+        await conn.sendBut(m.chat, `¡Ningún ausentismo en curso!`, wm, 'comienzo', `${usedPrefix}mulaiabsen`, m)
         throw false
     }
 
     let absen = conn.absen[id][1]
     const wasVote = absen.includes(m.sender)
-    if (wasVote) throw 'Kamu sudah absen!'
+    if (wasVote) throw 'estas ausente!'
     absen.push(m.sender)
     let d = new Date
     let date = d.toLocaleDateString('id', {
