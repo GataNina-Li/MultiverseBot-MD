4ae9d5b276316e6439123a5bc73dccf285c33cd1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-22 13:06:18 -0500

    Update info-bannedList.js

--- plugins/info-bannedList.js
+++ plugins/info-bannedList.js
@@ -1,25 +1,31 @@
 let handler = async (m, { conn, isOwner }) => {
     let chats = Object.entries(global.db.data.chats).filter(chat => chat[1].isBanned)
     let users = Object.entries(global.db.data.users).filter(user => user[1].banned)
-    let caption = `
-┌〔 Daftar Chat Terbanned 〕
-├ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
-├ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
-├ ${isOwner ? '@' + jid.split`@`[0] : jid}
+    
+m.reply(conn.sendBut(m.chat, `╭────────────────────🚫
+│ *Lista de chats prohibidos*
+│ *List of banned chats*
+│┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+│ Total : ${chats.length} Chat${chats ? '\n' + chats.map(([jid], i) => `
+│ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+│ ${isOwner ? '@' + jid.split`@`[0] : jid}
 `.trim()).join('\n') : ''}
-└────
-
-┌〔 Daftar Pengguna Terbanned 〕
-├ Total : ${users.length} Pengguna${users ? '\n' + users.map(([jid], i) => `
-├ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
-├ ${isOwner ? '@' + jid.split`@`[0] : jid}
+╰────────────────────🚫
+                          
+╭────────────────────🚫
+│ *Lista de Usuarios Prohibidos*
+│ *Banned Users List*
+│┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+│ Total : ${users.length} Pengguna${users ? '\n' + users.map(([jid], i) => `
+│ ${i + 1}. ${conn.getName(jid) == undefined ? 'Unknown' : conn.getName(jid)}
+│ ${isOwner ? '@' + jid.split`@`[0] : jid}
 `.trim()).join('\n') : ''}
-└────
-`.trim()
-    conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } })
+╰────────────────────🚫`, gt , `Menu`, '.menu',  m))
+    //`.trim())
+   //conn.reply(m.chat, caption, m, { contextInfo: { mentionedJid: conn.parseMention(caption) } }) separar contenido
 }
-handler.help = ['bannedlist']
+handler.help = ['listaban | listban',]
 handler.tags = ['info']
-handler.command = /^listban(ned)?|ban(ned)?list|daftarban(ned)?$/i
+handler.command = /^listban(ned)?|listaban|banlista|ban(ned)?list|daftarban(ned)?$/i
 handler.owner = false
 module.exports = handler
