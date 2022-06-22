332c18b8ee874d108f4fcedfd54aea205e37b9bc
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-22 13:23:25 -0600

    Traducido por sofia😏

--- plugins/anime-alphacoders.js
+++ plugins/anime-alphacoders.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn, text }) => {
-if (!text) return conn.reply(m.chat, 'Silahkan masukan query', m)
+if (!text) return conn.reply(m.chat, 'Por favor ingrese una consulta', m)
 let tetete = `https://api.zacros.my.id/search/alphacoders?query=${text}`
-conn.sendButtonImg(m.chat, tetete, `Hasil Dari ${text}`, wm2, 'Thanks', 'thanks', m) 
+conn.sendButtonImg(m.chat, tetete, `Resultados de ${text}`, wm2, 'Thanks', 'thanks', m) 
 }
 handler.help = ['alphacoders'].map(v => v + ' <query>')
 handler.tags = ['anime', 'internet']
