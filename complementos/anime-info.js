efbedf112aa74592f1917f0111ad9d63579c3551
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-22 13:42:03 -0600

    Traducido por sofia😏

--- complementos/anime-info.js
+++ complementos/anime-info.js
@@ -1,6 +1,6 @@
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan query!`
+  if (!text) throw `Introduce una consulta!`
   let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
   if (!res.ok) throw await res.text()
   let json = await res.json()
