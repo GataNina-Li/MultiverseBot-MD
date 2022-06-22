5e4dc491e24bd751442de7581fcfbe4a4acfe4b9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-22 18:03:28 -0500

    Update info-boost.js

--- plugins/info-boost.js
+++ plugins/info-boost.js
@@ -2,7 +2,7 @@ let { performance } = require('perf_hooks')
 
 let handler = async (m, { conn }) => {
 
- let start = `⚡ Boosting . . .`
+ let start = `⚡ Aumentar Velocidad`
  let boost = `${pickRandom(['0','1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20'])}%`
  let boost2 = `${pickRandom(['21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40'])}%`
  let boost3 = `${pickRandom(['41','42','43','44','45','46','47','48','49','50','51','52','53','54','55','56','57','58','59','60'])}%`
@@ -18,14 +18,14 @@ let handler = async (m, { conn }) => {
    let old = performance.now()
    let neww = performance.now()
    let speed = `${neww - old}`
-   let finish = `✔️Berhasil mempercepat Bot hingga\n${speed} milisec!`
-   let pp = 'https://c.tenor.com/XOJyPTMOUT8AAAAd/cosmos-universo.gif'
-   conn.sendButtonImg(m.chat, pp, finish, wm, 'Menu', '.menu', m)
+   let multi = `${ag}*SE AUMENTO LA VELOCIDAD*\n*HASTA* *_${speed}_* *milisegundos!*`
+   let pp = 'https://i.pinimg.com/originals/64/3a/81/643a81e32e243cd07bf37a93036769f5.jpg'
+   conn.sendButtonImg(m.chat, pp, multi, wm, '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡', '.menu', m)
   // conn.reply(m.chat, finish, m)
 }
-handler.help = ['boost', 'refresh']
+handler.help = ['masrapido | boost']
 handler.tags = ['info']
-handler.command = /^boost|refresh/i
+handler.command = /^boost|refresh|masrapido/i
 handler.mods = false
 handler.premium = false
 handler.group = false
