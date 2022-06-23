8da5e7bd995bb256d4de06f4a80dc15785885695
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 01:02:40 -0500

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -5,15 +5,75 @@
  * better bgtu dripada nma gw dihapus smua
  * najis 👎🗿
 */
-
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fetch = require('node-fetch')
+let fs = require('fs')
 let handler = async (m, { conn }) => {
 let ye = `@${m.sender.split`@`[0]}`
 let esce = `
-Hai ${ye} Lagi Nyari Sc Ya? 
+Hola!! ${ye} Aquí Te dejo Loa Desarroladores
+y más Información de MultiverseBot-MD
 
-_*Jangan Lupa Di Subscribe*_`
-conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
+*REPOSITORIO*
+- https://github.com/GataNina-Li/MultiverseBot-MD
+
+*DESARROLLADORES* 
+
+*ASISTENCIA* 
+
+*COLABORADORES*
+
+*COMANDOS DE INSTALACIÓN* 
+
+let message = await prepareWAMessageMedia({ video: fs.readFileSync('./media/menu/menuv3.mp4'), gifPlayback: true }, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           videoMessage: message.videoMessage,
+           hydratedContentText: esce,
+           hydratedFooterText: wm,
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '𝙂𝙞𝙩𝙃𝙪𝙗',
+               url: "https://github.com/GataNina-Li/MultiverseBot-MD"
+              }   
+            },
+              {
+              urlButton: {
+               displayText: '𝙄𝙣𝙨𝙩𝙖𝙜𝙧𝙖𝙢',
+               url: instagram	           
+             }
+           },
+			     {
+             quickReplyButton: {
+               displayText: '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡',
+               id: '.menu',
+             }
+	   },
+             {
+             quickReplyButton: {
+               displayText: '𝗠𝗘𝗡𝗨 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗢 💫',
+               id: '.? all',
+             }
+	   },
+             {
+             quickReplyButton: {
+               displayText: '𝗗𝗘𝗦𝗔𝗥𝗥𝗢𝗟𝗟𝗔𝗗𝗢𝗥𝗘𝗦  🛃',
+               id: '.menu',
+             }
+          }]
+        }
+      }
+    }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+    return await conn.relayMessage(
+         m.chat,
+         template.message,
+         { messageId: template.key.id }
+     )
 }
+//conn.sendBut(m.chat, esce, wm3, 'Thanks', 'thanks', m) 
+  
 handler.help = ['sc', 'sourcecode']
 handler.tags = ['info']
 handler.command = /^(sc|sourcecode)$/i
