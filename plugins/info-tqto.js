3189749e698379a1a16e8324ac646d4717813913
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 13:17:25 -0500

    Update info-tqto.js

--- plugins/info-tqto.js
+++ plugins/info-tqto.js
@@ -6,8 +6,23 @@
 
 let handler = async (m, { conn }) => {
 bear = "Source Code"
-let esce = `Ejemplo`
-conn.sendButtonImg(m.chat, fla + 'CREADORES', esce, wm, 'Menu', '.menu', m) 
+let esce = `*DESARROLLADORES* 
+⍟ https://github.com/GataNina-Li
+⍟ https://github.com/Abiguelreyes75
+⍟ https://github.com/Sofianayeli
+⍟ https://github.com/ADOLFO-BOT5
+
+*ASISTENCIA*
+⍟ https://www.instagram.com/gata_dios
+*Para Dudas, Sugerencias, Preguntas,*
+*Informate de las Novedades*
+
+*AGRADECIMIENTO A:*
+⍟ https://github.com/Hyzerr
+⍟ https://github.com/ilmanhdyt
+⍟ https://github.com/Nurutomo
+⍟ https://github.com/ariffb25
+⍟ https://github.com/Paquito1923`conn.sendButtonImg(m.chat, fla + 'CREADORES', esce, wm, '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡', '.menu', m) 
 }
 handler.help = ['creadores | creators']
 handler.tags = ['info']
