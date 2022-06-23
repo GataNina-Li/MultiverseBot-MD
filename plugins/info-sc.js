93c96b0a889ddc5acc948d981f6a6284e63e2fa1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 02:12:03 -0500

    Update info-sc.js

--- plugins/info-sc.js
+++ plugins/info-sc.js
@@ -85,7 +85,7 @@ let message = await prepareWAMessageMedia({ video: fs.readFileSync('./media/menu
                displayText: '𝗠𝗘𝗡𝗨 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗢 💫',
                id: '.? all',
              }
-	   }
+	   },
           }]
         }
       }
