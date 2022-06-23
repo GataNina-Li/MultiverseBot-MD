e80adfe3c232d63deabbc9a5e105cfbcaa589cd1
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 15:49:25 -0500

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -1,9 +1,7 @@
-//let { data } = await conn.getFile(await(await require('node-fetch')(pp)).buffer())
-const fs = require('fs')
-let handler = async (m, { text, chatId, options, conn }) => {
-/*const ftroli = {
+let handler = async (m, { conn }) => {
+const ftroli = {
     key : {
-    remoteJid: '51993042301@g.us',
+    remoteJid: '6283136505591-1614953337@g.us',
     participant : '0@s.whatsapp.net'
     },
     message: {
@@ -18,18 +16,9 @@ let handler = async (m, { text, chatId, options, conn }) => {
     }
     }
     }
-	conn.reply(m.chat, price1, ftroli)*/
+	conn.reply(m.chat, price1, ftroli)
+}
 
-
-let text = `
-*ejemplo`.trim()   
-conn.reply(m.chat, text, m, {
-contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
-title: 'bot',
-body: 'Super Bot WhatsApp',         
-previewType: 1, thumbnail: fs.readFileSync("./media/menu/shiraori.jpg"),
-sourceUrl: `${youtube}`}}})}  
-    	
 handler.help = ['sewa', 'sewabot']
 handler.tags = ['info']
 handler.command = /^(sewa|sewabot)?$/i
