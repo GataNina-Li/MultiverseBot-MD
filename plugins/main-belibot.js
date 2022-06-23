035eea6a747dbf2e46605b6fa1ac32cb69277bdd
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 16:53:40 -0500

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -1,4 +1,4 @@
-let fetch = require('node-fetch')
+//let fetch = require('node-fetch')
 let handler = async (m, { conn, chatId, options }) => {
 	
 	
@@ -71,13 +71,13 @@ const ftroli = {
 	//conn.reply(m.chat, `price1`, fkontak)
 
 //m.reply = async (text, chatId, options) => {
-    {	
+    	
 	    
 	let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg')
         let { data } = await conn.getFile(await(await require('node-fetch')(pp)).buffer())
 	let text = 'Hola'
         conn.reply(m.chat, text, m, { contextInfo: { mentionedJid: conn.parseMention(text), externalAdReply: { title: 'Super Bot Whatsapp', body: wm, sourceUrl: 'https://youtube.com/channel/UCpNU4eY7eiI0ve05CssjdbA', thumbnail: data }}, options })
-    }
+    
     m.name = m.pushName || conn.getName(m.sender)
    if (m.msg && m.msg.url) m.download = () => conn.downloadM(m.msg, m.mtype.toLowerCase().replace(/message/i, ''))
 	
