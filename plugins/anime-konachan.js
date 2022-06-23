c68a3b329ccc857dafb925082aff1404bd8a6056
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-22 23:57:28 -0500

    `Traducido por Sofia`

--- plugins/anime-konachan.js
+++ plugins/anime-konachan.js
@@ -1,7 +1,18 @@
-let handler = async(m, { conn, text }) => {
+/*let handler = async(m, { conn, text }) => {
 if (!text) return conn.reply(m.chat, 'Silahkan masukan query', m)
 let tetete = `https://api.zacros.my.id/search/konachan?query=${text}`
 conn.sendButtonImg(m.chat, tetete, `Hasil Dari ${text}`, wm2, 'Thanks', 'thanks', m) 
+}
+handler.help = ['konachan'].map(v => v + ' <query>')
+handler.tags = ['anime', 'internet']
+handler.command = /^(konachan)$/i
+
+module.exports = handler*/
+
+let handler = async(m, { conn, text }) => {
+if (!text) return conn.reply(m.chat, 'Por favor ingrese una consulta', m)
+let tetete = `https://api.zacros.my.id/search/konachan?query=${text}`
+conn.sendButtonImg(m.chat, tetete, `Resultados de ${text}`, wm2, 'Thanks', 'thanks', m) 
 }
 handler.help = ['konachan'].map(v => v + ' <query>')
 handler.tags = ['anime', 'internet']
