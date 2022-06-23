21fc09cddfc9e76510fae310dc3d49dce9f62546
ADOLFO SAN <104970935+ADOLFO-BOT5@users.noreply.github.com>
2022-06-23 03:38:05 -0500

    tools-tomp3.js

--- plugins/tools-tomp3.js
+++ plugins/tools-tomp3.js
@@ -4,7 +4,7 @@ const { MessageType } = require('@adiwajshing/baileys-md')
 let handler = async (m, { conn, usedPrefix, command }) => {
   let q = m.quoted ? m.quoted : m
   let mime = (m.quoted ? m.quoted : m.msg).mimetype || ''
-  if (!/video|audio/.test(mime)) throw `Balas video atau voice note yang ingin diubah ke mp3 dengan caption *${usedPrefix + command}*`
+  if (!/video|audio/.test(mime)) throw `Responda al video o nota de voz que desea convertir a mp3 con subtítulos *${usedPrefix + command}*`
   let media = await q.download()
   let audio = await toPTT(media, 'mp4')
  
