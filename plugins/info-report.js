f1cd7075deff4c507a3480c6df5feb71ea758d55
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-22 19:27:38 -0500

    Update info-report.js

--- plugins/info-report.js
+++ plugins/info-report.js
@@ -7,7 +7,7 @@ let handler = async(m, { conn, text, usedPrefix, command }) => {
     m.reply(laporan, jid)
     //m.reply(laporan, m.sender) // Mwehehehehe
     //m.reply('╰⊱💚⊱ *𝙀́𝙓𝙄𝙏𝙊 | 𝙎𝙐𝘾𝘾𝙀𝙎𝙎* ⊱💚⊱╮\n\n✔️!')
-  m.reply(conn.sendBut(m.chat, `${eg}*El reporte ha sido enviado a mí Creadora. Tendrá una respuesta pronto. De ser Falso será Ignorado el reporte.*\n*The report has been sent to my Creator. You will have an answer soon. If false, the report will be ignored.*`, gt , `☘ 𝙄𝙧 𝙖𝙡 𝙞𝙣𝙞𝙘𝙞𝙤 | 𝙂𝙤 𝙩𝙤 𝙨𝙩𝙖𝙧𝙩`, '.menu',  m))
+  m.reply(conn.sendBut(m.chat, `${eg}*El reporte ha sido enviado a mí Creadora. Tendrá una respuesta pronto. De ser Falso será Ignorado el reporte.*\n*The report has been sent to my Creator. You will have an answer soon. If false, the report will be ignored.*`, wm , `✨ 𝗠𝗘𝗡𝗨`, '.menu',  m))
 }
 handler.help = ['reporte | bug'].map(v => v + ' *comando con error*')
 handler.tags = ['info']
