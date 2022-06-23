d9271aca4e398a39377326f9c9603613749ea23f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 14:07:48 -0500

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -1,4 +1,6 @@
-let handler = async (m, { conn }) => {
+//let { data } = await conn.getFile(await(await require('node-fetch')(pp)).buffer())
+const fs = require('fs')
+let handler = async (m, { text, chatId, options, conn }) => {
 /*const ftroli = {
     key : {
     remoteJid: '51993042301@g.us',
@@ -19,14 +21,15 @@ let handler = async (m, { conn }) => {
 	conn.reply(m.chat, price1, ftroli)*/
 
 
- m.reply = async (text, chatId, options) => {
-    	let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg')
-        let { data } = await conn.getFile(await(await require('node-fetch')(pp)).buffer())
-        conn.reply(chatId ? chatId : m.chat, text, m, { contextInfo: { mentionedJid: conn.parseMention(text), externalAdReply: { title: 'Super Bot Whatsapp', body: wm, sourceUrl: 'https://youtube.com/channel/UCpNU4eY7eiI0ve05CssjdbA', thumbnail: data }}, options })
-    }
-    m.name = m.pushName || conn.getName(m.sender)
-    if (m.msg && m.msg.url) m.download = () => conn.downloadM(m.msg, m.mtype.toLowerCase().replace(/message/i, ''))
-}
+let text = `
+*ejemplo`.trim()   
+conn.reply(m.chat, text, m, {
+contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+title: 'bot',
+body: 'Super Bot WhatsApp',         
+previewType: 1, thumbnail: fs.readFileSync("./media/menu/shiraori.jpg"),
+sourceUrl: `${youtube}`}}})  
+    	
 handler.help = ['sewa', 'sewabot']
 handler.tags = ['info']
 handler.command = /^(sewa|sewabot)?$/i
