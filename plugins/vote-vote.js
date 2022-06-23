22daac91c5f345f1cf559e3f368223c24e99256e
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-22 23:54:12 -0500

    'ARREGLADO ERIKA'

--- plugins/vote-vote.js
+++ plugins/vote-vote.js
@@ -1,10 +1,10 @@
 let handler = async (m, { conn, usedPrefix, command }) => {
     let id = m.chat
     conn.vote = conn.vote ? conn.vote : {}
-    if (!(id in conn.vote)) return conn.sendBut(m.chat, `Tidak ada voting digrup ini!`, wm, 'Mulai', `${usedPrefix}+vote`, m)
+    if (!(id in conn.vote)) return conn.sendBut(m.chat, `Sin votar en este grupo!`, wm, 'comienzo', `${usedPrefix}+vote`, m)
     let isVote = conn.vote[id][1].concat(conn.vote[id][2])
     const wasVote = isVote.includes(m.sender)
-    if (wasVote) throw 'Kamu sudah vote!'
+    if (wasVote) throw 'has votado!'
     if (/up/i.test(command)) {
         conn.vote[id][1].push(m.sender)
     } else if (/de/i.test(command)) {
@@ -13,7 +13,7 @@ let handler = async (m, { conn, usedPrefix, command }) => {
     let [reason, upvote, devote] = conn.vote[id]
     let caption = `
 「 *Vote* 」
-*Alasan:* ${reason}
+*Razón:* ${reason}
 *Upvote*
 _Total: ${upvote.length}_
 ${upvote.map(u => '@' + u.split`@`[0]).join('\n')}
