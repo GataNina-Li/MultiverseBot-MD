27970484ecafa43412f1e6ec505a6edb62a589b2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 14:01:54 -0500

    Update main-belibot.js

--- plugins/main-belibot.js
+++ plugins/main-belibot.js
@@ -1,5 +1,5 @@
 let handler = async (m, { conn }) => {
-const ftroli = {
+/*const ftroli = {
     key : {
     remoteJid: '51993042301@g.us',
     participant : '0@s.whatsapp.net'
@@ -16,9 +16,17 @@ const ftroli = {
     }
     }
     }
-	conn.reply(m.chat, price1, ftroli)
-}
+	conn.reply(m.chat, price1, ftroli)*/
 
+
+ m.reply = async (text, chatId, options) => {
+    	let pp = await conn.profilePictureUrl(m.sender, 'image').catch(_ => 'https://telegra.ph/file/a2ae6cbfa40f6eeea0cf1.jpg')
+        let { data } = await conn.getFile(await(await require('node-fetch')(pp)).buffer())
+        conn.reply(chatId ? chatId : m.chat, text, m, { contextInfo: { mentionedJid: conn.parseMention(text), externalAdReply: { title: 'Super Bot Whatsapp', body: wm, sourceUrl: 'https://youtube.com/channel/UCpNU4eY7eiI0ve05CssjdbA', thumbnail: data }}, options })
+    }
+    m.name = m.pushName || conn.getName(m.sender)
+    if (m.msg && m.msg.url) m.download = () => conn.downloadM(m.msg, m.mtype.toLowerCase().replace(/message/i, ''))
+}
 handler.help = ['sewa', 'sewabot']
 handler.tags = ['info']
 handler.command = /^(sewa|sewabot)?$/i
