5cafc390242192fbe1016b0294e944817042e91d
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-22 22:52:14 -0500

    𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫

--- plugins/xp-regist.js
+++ plugins/xp-regist.js
@@ -2,25 +2,25 @@ const { createHash } = require('crypto')
 let Reg = /\|?(.*)([.|] *?)([0-9]*)$/i
 let handler = async function (m, { text, usedPrefix }) {
   let user = global.db.data.users[m.sender]
-  if (user.registered === true) throw `Anda sudah terdaftar\nMau daftar ulang? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
+  if (user.registered === true) throw `Ya estás registrado\¿Quieres volver a registrarte?? ${usedPrefix}unreg <SN|SERIAL NUMBER>`
   if (!Reg.test(text)) throw `Format salah\n*${usedPrefix}daftar nama.umur*`
   let [_, name, splitter, age] = text.match(Reg)
-  if (!name) throw 'Nama tidak boleh kosong (Alphanumeric)'
-  if (!age) throw 'Umur tidak boleh kosong (Angka)'
+  if (!name) throw 'Los nombres no pueden estar vacíos (Alphanumeric)'
+  if (!age) throw 'La edad no puede estar vacía (Número)'
   age = parseInt(age)
-  if (age > 120) throw 'Umur terlalu tua 😂'
-  if (age < 5) throw 'Bayi bisa ngetik sesuai format bjir ._.'
+  if (age > 120) throw 'edad demasiado vieja 😂'
+  if (age < 5) throw 'Los bebés pueden escribir según el poder galáctico'
   user.name = name.trim()
   user.age = age
   user.regTime = + new Date
   user.registered = true
   let sn = createHash('md5').update(m.sender).digest('hex')
   m.reply(`
-Daftar berhasil!
+Regístro 🌌 galactico 🌌 con éxito!
 
-╭─「 Info 」
+╭─「🌌 Info 🌌」
 │ Nama: ${name}
-│ Umur: ${age} tahun 
+│ Umur: ${age} año 
 ╰────
 Serial Number: 
 ${sn}
