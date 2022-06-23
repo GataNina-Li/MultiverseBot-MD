d694dafb7930fc62bd4fab4b023262714c75046f
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-22 23:45:50 -0500

    Update anime-info.js

--- plugins/anime-info.js
+++ plugins/anime-info.js
@@ -1,28 +1,3 @@
-/*let fetch = require('node-fetch')
-let handler = async(m, { conn, text }) => {
-  if (!text) throw `Masukkan query!`
-  let res = await fetch(global.API('https://api.jikan.moe', '/v3/search/anime', { q: text }))
-  if (!res.ok) throw await res.text()
-  let json = await res.json()
-  let { title, members, synopsis, episodes, url, rated, score, image_url, type, start_date, end_date } = json.results[0]
-let animeingfo = `✨️ *Title:* ${title}
-🎆️ *Episodes:* ${episodes}
-➡️ *Start date:* ${start_date}
-🔚 *End date:* ${end_date}
-💬 *Show Type:* ${type}
-💌️ *Rating:* ${rated}
-❤️ *Score:* ${score}
-👥 *Members:* ${members}
-💚️ *Synopsis:* ${synopsis}
-🌐️ *URL*: ${url}`
-  conn.sendFile(m.chat, image_url, '', animeingfo, m)
-}
-handler.help = ['anime <judul>']
-handler.tags = ['anime']
-handler.command = /^(anime|animeinfo)$/i
-//maapin fatur :<
-module.exports = handler*/
-
 let fetch = require('node-fetch')
 let handler = async(m, { conn, text }) => {
   if (!text) throw `Introduce una consulta!`
@@ -42,7 +17,7 @@ let animeingfo = `✨️ Title: ${title}
 🌐️ URL: ${url}`
   conn.sendFile(m.chat, image_url, '', animeingfo, m)
 }
-handler.help = ['anime <judul>']
+handler.help = ['anime <title>']
 handler.tags = ['anime']
 handler.command = /^(anime|animeinfo)$/i
 //maapin fatur :<
