dcd857d3f09aef0e22a913eaef8495d508300471
ADOLFO SAN <104970935+ADOLFO-BOT5@users.noreply.github.com>
2022-06-23 03:42:06 -0500

    tools-topdf.js

--- plugins/tools-topdf.js
+++ plugins/tools-topdf.js
@@ -4,11 +4,11 @@ const uploadImage = require('../lib/uploadImage')
 let handler = async (m) => {
   let q = m.quoted ? m.quoted : m
   let mime = (q.msg || q).mimetype || ''
-  if (!mime) throw 'Tidak ada media yang ditemukan'
+  if (!mime) throw 'No se encontraron medios'
   let media = await q.download()
   let isTele = /image\/(png|jpe?g|gif)|video\/mp4/.test(mime)
   let link = await (isTele ? uploadImage : uploadFile)(media)
-  conn.sendFile(m.chat, `https://api.xteam.xyz/imagetopdf?url=${link}&APIKEY=cristian9407`, 'NihKak', null, m)
+  conn.sendFile(m.chat, `https://api.xteam.xyz/imagetopdf?url=${link}&APIKEY=cristian9407`, 'MultiverseBot', null, m)
 }
 handler.help = ['topdf <reply image>']
 handler.tags = ['tools']
