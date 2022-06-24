b7ee295d57db4ce247b5ee0f4042bd981a99752e
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-24 01:43:41 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,19 +1,20 @@
-const { facebook } = require('../lib/facebook')
-let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `uhm.. url nya mana?\n\ncontoh:\n${usedPrefix + command} https://www.facebook.com/alanwalkermusic/videos/277641643524720`
-  if (!args[0].match(/https:\/\/.*(facebook.com|fb.watch)/gi)) throw `url salah`
-  facebook(args[0]).then(async res => {
-    let fb = JSON.stringify(res)
-    let json = JSON.parse(fb)
-    // m.reply(require('util').format(json))
+let fetch = require('node-fetch')
+
+let handler = async (m, { conn, command, args, usedPrefix }) => {
+    if (!args[0]) return m.reply('Putting *URL* Facebook..')
+    if (!args[0].includes("facebook")) return m.reply(`Url is wrong..\n\n*Example:*\n${usedPrefix}fb https://www.facebook.com/juankcortavarriaoficial/videos/218237676749570/`)
+    let res = await fetch(`https://api.neoxr.eu.org/api/fb?url=${args[0]}&apikey=obSw1DxesD`)
+   
+    let json = await res.json()
     if (!json.status) throw json
-    await m.reply(global.wait)
-    await conn.sendFile(m.chat, json.data[0].url, '', '© shiraori', m)
-  }).catch(_ => _)
+    await m.reply('Sedang di proses..')
+    await conn.sendFile(m.chat, json.data[1].url, json.data[1].url, wm, m)
 }
-handler.help = ['fb'].map(v => v + ' <url>')
+handler.help = ['fb2'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+handler.command = /^f((b|acebook|)(dl|download)?(er)?(2)?)$/i
+handler.limit = true
+handler.group = true
 
 module.exports = handler
