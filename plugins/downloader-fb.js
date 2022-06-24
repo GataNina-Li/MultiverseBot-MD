3c1bd498c6dd80f41cde05b3096c4db2f3b29a8c
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 01:24:16 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,41 +1,14 @@
-const cheerio = require('cheerio')
-const fetch = require('node-fetch')
-const FormData = require('form-data')
-let handler = async (m, { usedPrefix, command, conn, args }) => {
-  if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} https://fb.watch/xxxxx/`
-  let res = await fbdl(args[0])
-  if (!res['mp4'].length) throw 'Video not found'
-  let video, index
-  for (let i = res['mp4'].length - 1; i > 0; i--) {
-    let { link, form } = res['mp4'][i]
-    let err
-    try {
-      let res = await fetch(link, {
-        method: 'POST',
-        headers: { ...form.getHeaders() },
-        body: form
-      })
-      if (!res.ok) continue
-      video = await res.buffer()
-      index = i
-      break
-    } catch (e) {
-      err = e
-      continue
-    }
-    throw err
-  }
-  if (!video) throw 'Can\t get video'
-  let json = res['mp4'][index]
-  let caption = `
-*Size:* ${json.fileSizeF}
-*Kualitas:* ${json.quality}
-`.trim()
-  conn.sendFile(m.chat, video, 'media-fb', caption, m)
-}
-handler.help = ['fb'].map(v => v + ' <url>')
+let { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `enlace mal`
+try {
+const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
+} catch (e) {
+await m.reply('enlace ?')
+console.log(e)
+}}
+handler.help = ['facebbok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
-
-module.exports = handler
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
+export default handler
