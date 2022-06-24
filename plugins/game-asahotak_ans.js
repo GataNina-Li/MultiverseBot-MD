ad66bb867b1b1f209294d18c1d11c769d34b99ed
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 01:34:23 -0500

    `Traducido por Sofia`

--- plugins/game-asahotak_ans.js
+++ plugins/game-asahotak_ans.js
@@ -1,4 +1,4 @@
-const similarity = require('similarity')
+/*const similarity = require('similarity')
 const threshold = 0.72
 let handler = m => m
 handler.before = async function (m) {
@@ -16,6 +16,32 @@ handler.before = async function (m) {
             delete this.asahotak[id]
         } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`*Dikit Lagi!*`)
         else m.reply(`*Salah!*`)
+    }
+    return !0
+}
+handler.exp = 0
+
+module.exports = handler*/
+
+
+const similarity = require('similarity')
+const threshold = 0.72
+let handler = m => m
+handler.before = async function (m) {
+    let id = m.chat
+    if (!m.quoted || !m.quoted.fromMe || !m.quoted.isBaileys || !/Ketik.*ao/i.test(m.quoted.contentText)) return !0
+    this.asahotak = this.asahotak ? this.asahotak : {}
+    if (!(id in this.asahotak)) return m.reply('el asunto ha terminado')
+    if (m.quoted.id == this.asahotak[id][0].id) {
+        let json = JSON.parse(JSON.stringify(this.asahotak[id][1]))
+        if (['.ao', 'Ayuda', ''].includes(m.text)) return !0
+        if (m.text.toLowerCase() == json.jawaban.toLowerCase().trim()) {
+            global.db.data.users[m.sender].exp += this.asahotak[id][2]
+            await this.sendBut(m.chat, `Derecha! +${this.asahotak[id][2]} XP`, 'por elyas', 'Ejercicio mental', '.asahotak', m)
+            clearTimeout(this.asahotak[id][3])
+            delete this.asahotak[id]
+        } else if (similarity(m.text.toLowerCase(), json.jawaban.toLowerCase().trim()) >= threshold) m.reply(`un poco más!`)
+        else m.reply(`Equivocado!`)
     }
     return !0
 }
