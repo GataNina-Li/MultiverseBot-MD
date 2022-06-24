a8ebf5d19fb87c5c1da2ff9c465bc2402b6fbe98
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 02:06:04 -0500

    `Traducido por Sofia`

--- plugins/game-caklontong.js
+++ plugins/game-caklontong.js
@@ -1,4 +1,4 @@
-let fetch = require('node-fetch')
+/*let fetch = require('node-fetch')
 
 let timeout = 120000
 let poin = 500
@@ -30,6 +30,43 @@ Bonus: ${poin} XP
 }
 handler.help = ['caklontong']
 handler.tags = ['game']
+handler.command = /^caklontong/i */
+
+
+let fetch = require('node-fetch')
+
+let timeout = 120000
+let poin = 500
+let src
+let handler = async (m, { conn, usedPrefix }) => {
+    conn.caklontong = conn.caklontong ? conn.caklontong : {}
+    let id = m.chat
+    if (id in conn.caklontong) {
+        conn.reply(m.chat, 'Todavía hay preguntas sin respuesta en este chat.', conn.caklontong[id][0])
+        throw false
+    }
+    if (!src) src = await (await fetch('https://raw.githubusercontent.com/BochilTeam/database/master/games/caklontong.json')).json()
+    let json = src[Math.floor(Math.random() * src.length)]
+    let caption = `
+${json.soal}
+
+Timeout ${(timeout / 1000).toFixed(2)} detik
+Ketik ${usedPrefix}calo untuk bantuan
+Bonus: ${poin} XP
+`.trim()
+    conn.caklontong[id] = [
+        await conn.sendBut(m.chat, caption, author, 'Ayuda', '.Soltar', m),
+        json, poin,
+        setTimeout(async () => {
+            if (conn.caklontong[id]) conn.sendBut(m.chat, `el tiempo se ha acabado!\nLa respuesta es ${json.jawaban}\n${json.deskripsi}`, wm, 'Cak Lontong', '.caklontong', conn.caklontong[id][0])
+            delete conn.caklontong[id]
+        }, timeout)
+    ]
+}
+handler.help = ['caklontong']
+handler.tags = ['game']
 handler.command = /^caklontong/i
+
+module.exports = handler
 
 module.exports = handler
