c44882e239496b60fbd27a0dac92f1a00bb07e53
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 23:26:44 -0500

    𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫

--- plugins/info-statusbot.js
+++ plugins/info-statusbot.js
@@ -100,7 +100,7 @@ let esce =
 handler.help = ['estacion | orbita']
 handler.tags = ['info']
 handler.command = /^órbita|orbita|orvita|estación|estacion|estacion|estado|status|estate|state|stado|stats|botstat(us)?$/i
-handler.exp = 4 
+handler.exp = 2
 
 module.exports = handler
 
@@ -111,3 +111,8 @@ function clockString(ms) {
     return [h, m, s].map(v => v.toString().padStart(2, 0)).join(':')
 	
     }
+
+// SI VAS A MODIFICAR AL MENOS NO QUITES LOS CREDITOS, WEB, ENLACES, DE ESA FORMA YA ESTAS APOYANDO 
+// PARA QUE SE PUEDA SEGUIR ELABORANDO EN ESTE LINDO MULTIVERSEBOT-MD 
+// TAMBIEN PUEDES AGREGAR TÚ INFORMACIÓN SIN ESTAR ELIMINANDO LOS CREDITOS 
+// EN CASO DE PODER COMPARTIR ESTE REPOSITORIO SE TE AGREDECE, Y GRACIAS POR INTERESARTE EN MULTIVERSEBOT-MD!! 
