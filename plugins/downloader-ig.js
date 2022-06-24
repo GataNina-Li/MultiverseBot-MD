c2ae45262191e0622ab94ffedb970eff73b0ceac
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 03:12:05 -0500

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -2,15 +2,31 @@ const { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } = require('@b
 
 //import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-if (!args[0]) throw `Enlace?`
+
+  const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+} 
+  
+if (!args[0]) throw `${mg}*Introduce un enlace valido de Instagram, para descargar La imagen o Vídeo*\n\n*EJEMPLO*\n*${usedPrefix + command}* _https://www.instagram.com/tv/CfHCqLDAg0o/?utm_source=ig_web_copy_link_`
 const results = await instagramdl(args[0])
 .catch(async _ => await instagramdlv2(args[0]))
 .catch(async _ => await instagramdlv3(args[0]))
 .catch(async _ => await instagramdlv4(args[0]))
-for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
+for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `⚡ *Url:* ${url}`, fkontak, m)
 }
-handler.help = ['ig'].map(v => v + ' <url>')
+handler.help = ['ig | ighistoria', 'instagram | igreels'].map(v => v + ' *enlace*')
 handler.tags = ['downloader']
-handler.command =/^(instagram|ig(dl)?)$/i
+handler.command =/^(instagram|ighistory(s)|ighistoria(s)|igreels|ig(dl)?)$/i
 
 module.exports = handler
