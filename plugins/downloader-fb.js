ce88bf67e6609e7c396cd93ed83edbc9de15495f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 01:04:10 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,17 +1,27 @@
-let xfar = require('xfarr-api')
 let fetch = require('node-fetch')
-let handler = async (m, { conn, command, text }) => {
-    if (!text) throw 'Masukkan Link\n\nContoh: .fb https://facebook.com/xxxxxx'
-  let res = await xfar.Facebook(text)
-m.reply('*Tunggu Sebentar...*')
-conn.sendFile(m.chat,res.medias[1].url, 'fb.mp4', `Tittle: ${res.tittle}
-Link Video: ${res.url}
-`, m)
+const {
+    MessageType
+} = require('@adiwajshing/baileys')
 
-}
-handler.help = ['fb <url>', 'facebook <url>']
+let handler = async (m, { conn, args, usedPrefix }) => {
+	if (!args[0]) return m.reply('Putting *URL* Facebook..')
+	let res = await fetch(`https://masgimenz.my.id/facebook/?url=` + args[0])
+	//if (res.status !== 200) throw `Coba Lagi`
+	let json = await res.json()
+	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
+	let url = json.videoUrl
+	m.reply('Sedang diproses...')
+	if (url) await conn.sendFile(m.chat, url, 'fb.mp4', wm, m)
+	//if (url) await conn.sendMessage(m.chat, url, MessageType.video, {mimetype: 'video/mp4', quoted: m, caption: wm})
+	else m.reply('Link download tidak ditemukan')
+	}
+
+handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^fb|facebook$/i
 
+handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+handler.limit = true
+handler.group = true
+handler.premium = false
 
 module.exports = handler
