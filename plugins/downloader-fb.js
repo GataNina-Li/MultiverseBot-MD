52aff1881a742adaee5b9bf2cad98a4e8a3c1565
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-24 02:39:06 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,14 +1,42 @@
-
-  let form = new FormData()
-  form.append('action', 'wsmd_user_download_request')
-  form.append('link', encodeURI(url))
-  form.append('requestlink', 'https://video-converter-mp4.com/fb-downloader/')
-  form.append('deftab', 'video')
-  let res = await fetch('https://video-converter-mp4.com/wp-admin/admin-ajax.php', {
-  })
- handler.help = ['fb'].map(v => v + ' <url>')
+process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
+const { servers, yta, ytv } = require('../lib/y3mate')
+let yts = require('yt-search')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text, usedPrefix }) => {
+  if (!text) throw `🎁 *Ingrese un texto o enlace de YT*\n\n*✅ Ejemplo:*\n*${usedPrefix + command} Bellyache*`
+//  let chat = global.db.data.chats[m.chat]
+  let results = await yts(text)
+  let vid = results.all.find(video => video.seconds < 3600)
+  if (!vid) throw '*El video no se encontró, intente ingresar el nombre original de la canción o video*'
+  let isVideo = /2$/.test(command)
+  let yt = false
+  let yt2 = false
+  let usedServer = servers[0]
+  m.reply('🔁 *Descargando...*\n\n*❰ ❗ ❱ Si no obtiene ningun resultado o le sale algun error intente con otro nombre*')
+  for (let i in servers) {
+    let server = servers[i]
+    try {
+      yt = await yta(vid.url, server)
+      yt2 = await ytv(vid.url, server)
+      usedServer = server
+      break
+    } catch (e) {
+      m.reply(`*El servidor ${server} fallo!, reintentando con otro servidor*${servers.length >= i + 1 ? '' : '\nmencoba server lain...'}`)
+    }
+  }
+  if (yt === false) throw '*Todos los servidores fallaron*'
+  if (yt2 === false) throw '*Todos los servidores fallaron*'
+  let { dl_link, thumb, title, filesize, filesizeF } = yt
+  await conn.send2ButtonLoc(m.chat, await (await fetch(thumb)).buffer(), `
+*💗 Titulo:* _${title}_
+*🎈 Peso del audio:* _${filesizeF}_
+*🎈 Peso del video:* _${yt2.filesizeF}_
+`.trim(), 'Gata Dios', '🎵 AUDIO', `.yta ${vid.url}`, '🎥 VIDEO', `.yt ${vid.url}`)
+}
+handler.help = ['play'].map(v => v + ' <pencarian>')
 handler.tags = ['downloader']
+handler.command = /^(reproducir|reproducir2|reproductor|Reproducir|Reproducir2|Reproductor|play3|Play3|playvid|Playvid|playaudio|Playaudio)$/i
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+handler.exp = 0
 
-module.exports = handler
+module.exports = handle
