2404c7bce8f2f80cc0c3570ce42e6cd26bd14720
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-24 01:32:37 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,16 +1,17 @@
+let xfar = require('xfarr-api')
+let fetch = require('node-fetch')
+let handler = async (m, { conn, command, text }) => {
+    if (!text) throw 'Masukkan Link\n\nContoh: .fb https://facebook.com/xxxxxx'
+  let res = await xfar.Facebook(text)
+m.reply('*Tunggu Sebentar...*')
+conn.sendFile(m.chat,res.medias[1].url, 'fb.mp4', `Tittle: ${res.tittle}
+Link Video: ${res.url}
+`, m)
 
-let handler = async (m, { conn, args }) => {
-let { facebookdl, facebookdlv2 } require('@bochilteam/scraper')
-  
-if (!args[0]) throw `enlace mal`
-try {
-const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `🔗 *Url:* ${url}`, m)
-} catch (e) {
-await m.reply('enlace ?')
-console.log(e)
-}}
-handler.help = ['facebbok'].map(v => v + ' <url>')
+}
+handler.help = ['fb <url>', 'facebook <url>']
 handler.tags = ['downloader']
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i
-export default handler
+handler.command = /^fb|facebook$/i
+
+
+module.exports = handler
