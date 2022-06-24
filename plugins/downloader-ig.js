2699cd6fe06e84b33f14c4ae49a88b591f17222b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 02:24:40 -0500

    Update downloader-ig.js

--- plugins/downloader-ig.js
+++ plugins/downloader-ig.js
@@ -1,21 +1,16 @@
-const { igdl } = require('../lib/scrape')
+const { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } = require('@bochilteam/scraper')
 
+//import { instagramdl, instagramdlv2, instagramdlv3, instagramdlv4 } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-
-  if (!args[0]) throw `Pengunaan:\n${usedPrefix + command} <url>\n\nContoh:\n${usedPrefix + command} https://www.instagram.com/p/CQU21b0JKwq/`
-  if (!args[0].match(/https:\/\/www.instagram.com\/.*(p|reel|tv)/gi)) throw `url salah, perintah ini untuk mengunduh post/reel/tv`
-
-  igdl(args[0]).then(async res => {
-    let igdl = JSON.stringify(res)
-    let json = JSON.parse(igdl)
-    await m.reply(global.wait)
-    for (let { downloadUrl, type } of json) {
-      conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '*nih*', m)
-    }
-  })
+if (!args[0]) throw `Enlace?`
+const results = await instagramdl(args[0])
+.catch(async _ => await instagramdlv2(args[0]))
+.catch(async _ => await instagramdlv3(args[0]))
+.catch(async _ => await instagramdlv4(args[0]))
+for (const { url } of results) await conn.sendFile(m.chat, url, 'instagram.mp4', `🔗 *Url:* ${url}`, m)
 }
 handler.help = ['ig'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^(ig|instagram)$/i
+handler.command =/^(instagram|ig(dl)?)$/i
 
 module.exports = handler
