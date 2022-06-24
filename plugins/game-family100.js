867bb01fcc6189261901f0c2cffe9436e7c106f5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 02:12:45 -0500

    `Traducido por Sofia`

--- plugins/game-family100.js
+++ plugins/game-family100.js
@@ -1,4 +1,4 @@
-let fetch = require('node-fetch')
+/*let fetch = require('node-fetch')
 let winScore = 500
 async function handler(m) {
     this.game = this.game ? this.game : {}
@@ -15,6 +15,43 @@ async function handler(m) {
 *Soal:* ${json.soal}
 
 Terdapat *${json.jawaban.length}* jawaban${json.jawaban.find(v => v.includes(' ')) ? `
+(beberapa jawaban terdapat spasi)
+`: ''}
+
++${winScore} XP tiap jawaban benar
+    `.trim()
+    this.game[id] = {
+        id,
+        msg: await m.reply(caption),
+        ...json,
+        terjawab: Array.from(json.jawaban, () => false),
+        winScore,
+    }
+}
+handler.help = ['family100']
+handler.tags = ['game']
+handler.command = /^family100$/i
+
+module.exports = handler*/
+
+
+let fetch = require('node-fetch')
+let winScore = 500
+async function handler(m) {
+    this.game = this.game ? this.game : {}
+    let id = 'family100_' + m.chat
+    if (id in this.game) {
+        this.reply(m.chat, 'Todavía hay cuestionarios sin responder en este chat.', this.game[id].msg)
+        throw false
+    }
+    let res = await fetch(global.API('xteam', '/game/family100', {}, 'APIKEY'))
+    if (!res.ok) throw await res.text()
+    let json = await res.json()
+    if (!json.status) throw json
+    let caption = `
+Soal: ${json.soal}
+
+Terdapat ${json.jawaban.length} jawaban${json.jawaban.find(v => v.includes(' ')) ? `
 (beberapa jawaban terdapat spasi)
 `: ''}
 
