dece2657244086d89d06f2df545b2e3eb97cffd2
Sofianayeli <107886031+Sofianayeli@users.noreply.github.com>
2022-06-23 19:43:33 -0500

    Add files via upload

---
+++ plugins/onwer-banchat.js
@@ -0,0 +1,30 @@
+let handler = async (m, { conn, isOwner, text, isAdmin }) => {
+  let who
+  if (m.isGroup) {
+    if (!(isAdmin || isOwner)) {
+      global.dfail('admin', m, conn)
+      throw false
+    }
+    if (isOwner) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+    else who = m.chat
+  } else {
+    if (!isOwner) {
+      global.dfail('owner', m, conn)
+      throw false
+    }
+    who = text ? text.replace(/[^0-9]/g, '') + '@s.whatsapp.net' : m.chat
+  }
+
+  try {
+    if (who.endsWith('g.us')) global.db.data.chats[who].isBanned = true
+    else global.db.data.users[who].banned = true
+    m.reply(`Prohibir el éxito! ${await conn.user.name} no activo en el chat ${await conn.getName(who) == undefined ? 'ini' : await conn.getName(who)}.`)
+  } catch (e) {
+    throw `¡El número no existe en la base de datos!`
+  }
+}
+handler.help = ['ban']
+handler.tags = ['owner', 'group']
+handler.command = /^ban(chat)?$/i
+
+module.exports = handler
