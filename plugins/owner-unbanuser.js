fc4d8414b04b91cac66d1971645e5e1138153df4
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 20:35:43 -0500

    Update owner-unbanuser.js

--- plugins/owner-unbanuser.js
+++ plugins/owner-unbanuser.js
@@ -3,12 +3,12 @@ let handler = async (m, { conn, text }) => {
     let who
     if (m.isGroup) who = m.mentionedJid[0]
     else who = m.chat
-    if (!who) throw 'Tag salah satu lah'
+    if (!who) throw 'Etiqueta uno'
     let users = global.db.data.users
     users[who].banned = false
     conn.reply(m.chat, `Desban con exito`, m)
 }
-handler.help = ['ban']
+handler.help = ['unban']
 handler.tags = ['owner']
 handler.command = /^unban$/i
 handler.mods = true
