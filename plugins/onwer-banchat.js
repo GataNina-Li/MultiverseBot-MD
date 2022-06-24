5ffd17d032769550b5c2c33ae19b96ce680f9b0f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-23 20:36:52 -0500

    Update onwer-banchat.js

--- plugins/onwer-banchat.js
+++ plugins/onwer-banchat.js
@@ -25,6 +25,6 @@ let handler = async (m, { conn, isOwner, text, isAdmin }) => {
 }
 handler.help = ['ban']
 handler.tags = ['owner', 'group']
-handler.command = /^ban(chat)?$/i
+handler.command = /^banchat$/i
 
 module.exports = handler
