7802a03d0bbab4dffa7e26a2881b9dac2500ad45
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-24 02:02:39 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,51 +1,37 @@
-let fetch = require('node-fetch')
+const {MessageType, Mimetype, MessageOptions} = require('@adiwajshing/baileys');
+const Asena = require('../events');
+const Config = require('../config');
+const WhatsAsenaStack = require('whatsasena-npm');
+const axios = require('axios')
+let wk = Config.WORKTYPE == 'public' ? false : true
 
-let handler = async (m, { conn, args }) => {
-  if (!args[0]) throw 'Uhm...url nya mana?'
-  let res = await fetch(global.API('xteam', '/dl/fb', {
-    url: args[0]
-  }, 'APIKEY'))
-  if (res.status !== 200) {
-    res.text()
-    throw res.status
-  }
-  let json = await res.json()
-  if (!json.result) throw json
-  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
-  let { name: authorname, url: authorlink } = author || {}
-  let dateConfig = {
-    hour: 'numeric',
-    minute: 'numeric',
-    second: 'numeric',
-    day: 'numeric',
-    month: 'long',
-    year: 'numeric'
-  }
-  let unknown = '_Unknown_'
-  let none = '_None_'
-  let caption = `
-Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
-Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
-Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
-Size: ${contentSize || unknown}
-Durasi: ${clockString(+ new Date(duration))}
-Genre: ${genre || none}
-Kualitas: ${videoQuality ? videoQuality : unknown}
-${description}
-Keyword: ${keywords || none}
-`.trim()
-  conn.sendFile(m.chat, url, 'media-fb', caption, m)
-}
-handler.help = ['fb'].map(v => v + ' <url>')
-handler.tags = ['downloader']
+var CLR_DESC = ''
+var wr = ''
+if (Config.LANG == 'TR') CLR_DESC = 'Facebook üzeriden video indirir.', wr = '*Lütfen Geçerli Bir Video Bağlantısı Girin!*'
+if (Config.LANG == 'AZ') CLR_DESC = 'Facebookdan video yükləyir.', wr = '*Zəhmət olmasa Etibarlı Video Bağlantısı daxil edin!*'
+if (Config.LANG == 'EN') CLR_DESC = 'Downloads videos from Facebook.', wr = '*Please Enter a Valid Video Link!*'
+if (Config.LANG == 'PT') CLR_DESC = 'Baixa vídeos do Facebook.', wr = '*Insira um link de vídeo válido!*'
+if (Config.LANG == 'RU') CLR_DESC = 'Скачивает видео с Facebook.', wr = '*Пожалуйста, введите действительную ссылку на видео!*'
+if (Config.LANG == 'HI') CLR_DESC = 'फेसबुक से वीडियो डाउनलोड करता है।', wr = '*कृपया एक वैध वीडियो लिंक दर्ज करें!*'
+if (Config.LANG == 'ES') CLR_DESC = 'Descarga videos de Facebook.', wr = '*¡Ingrese un enlace de video válido!*'
+if (Config.LANG == 'ML') CLR_DESC = 'Facebook വീഡിയോകൾ ഡൗൺലോഡ് ചെയ്യുന്നു.', wr = '*സാധുവായ ഒരു വീഡിയോ ലിങ്ക് നൽകുക!*'
+if (Config.LANG == 'ID') CLR_DESC = 'Mengunduh video dari Facebook.', wr = '*Silakan Masukkan Tautan Video yang Valid!*'
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+Asena.addCommand({pattern: 'fb ?(.*)', fromMe: wk, desc: CLR_DESC, usage: 'fb https://www.facebook.com/Google/videos/10156367314197838'}, (async (message, match) => {
+  var reg = new RegExp(/^http(?:s?):\/\/(?:www\.|web\.|m\.)?facebook\.com\/([A-z0-9\.]+)\/videos(?:\/[0-9A-z].+)?\/(\d+)(?:.+)?$/, 'gm')
+  var is_valid = reg.test(match[1])
+  if (!is_valid) return await message.client.sendMessage(message.jid, wr, MessageType.text)
+  var payload = await WhatsAsenaStack.facebook(match[1])
+  var auth_message = await WhatsAsenaStack.facebook_message(Config.LANG)
 
-module.exports = handler
-
-function clockString(ms) {
-  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
-  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
-  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
-  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
-}
+  var video = await axios.get(payload.video, { responseType: 'arraybuffer'})
+  var caption_message = auth_message.username + payload.username + '\n' +
+    auth_message.title + payload.title + '\n' +
+    auth_message.caption + payload.caption + '\n' +
+    auth_message.play + payload.plays + '\n' +
+    auth_message.like + payload.like + '\n' +
+    auth_message.share + payload.share + '\n' +
+    auth_message.comment + payload.comment + '\n' +
+    auth_message.creation + payload.created_at
+  await message.sendMessage(Buffer.from(video.data), MessageType.video, { caption: caption_message, mimetype: Mimetype.mp4 })
+}));
