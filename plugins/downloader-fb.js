8600566ed7a792337321fe1b01332c315851d9ba
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 01:07:47 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,27 +1,53 @@
 let fetch = require('node-fetch')
-const {
-    MessageType
-} = require('@adiwajshing/baileys')
 
-let handler = async (m, { conn, args, usedPrefix }) => {
-	if (!args[0]) return m.reply('Putting *URL* Facebook..')
-	let res = await fetch(`https://masgimenz.my.id/facebook/?url=` + args[0])
-	//if (res.status !== 200) throw `Coba Lagi`
-	let json = await res.json()
-	//if (!json.result) throw `Media tidak ditemukan atau postingan mungkin diprivate`
-	let url = json.videoUrl
-	m.reply('Sedang diproses...')
-	if (url) await conn.sendFile(m.chat, url, 'fb.mp4', wm, m)
-	//if (url) await conn.sendMessage(m.chat, url, MessageType.video, {mimetype: 'video/mp4', quoted: m, caption: wm})
-	else m.reply('Link download tidak ditemukan')
-	}
+let handler = async (m, { conn, args }) => {
+  if (!args[0]) throw 'Uhm...url nya mana?'
+  let res = await fetch(global.API('xteam', '/dl/fb', {
+    url: args[0]
+  }, 'APIKEY'))
+  if (res.status !== 200) {
+    res.text()
+    throw res.status
+  }
+  let json = await res.json()
+  if (!json.result) throw json
+  let { name, author, description, uploadDate, duration, url, isFamilyFriendly, genre, keywords, contentSize, videoQuality, commentCount } = json.result
+  let { name: authorname, url: authorlink } = author || {}
+  let dateConfig = {
+    hour: 'numeric',
+    minute: 'numeric',
+    second: 'numeric',
+    day: 'numeric',
+    month: 'long',
+    year: 'numeric'
+  }
+  let unknown = '_Unknown_'
+  let none = '_None_'
+  let caption = `
+Konten${isFamilyFriendly ? ' ' : ' *Tidak* '}Family Friendly
+Post oleh ${name} (${authorname || ''}) (${authorlink || ''})
+Diposting pada ${new Date(uploadDate).toLocaleDateString('id', dateConfig)}
+Size: ${contentSize || unknown}
+Durasi: ${clockString(+ new Date(duration))}
+Genre: ${genre || none}
+Kualitas: ${videoQuality ? videoQuality : unknown}
 
+${description}
+
+Keyword: ${keywords || none}
+`.trim()
+  conn.sendFile(m.chat, url, 'media-fb', caption, m)
+}
 handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 
 handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
-handler.limit = true
-handler.group = true
-handler.premium = false
 
 module.exports = handler
+
+function clockString(ms) {
+  let h = isNaN(ms) ? '--' : Math.floor(ms / 3600000)
+  let m = isNaN(ms) ? '--' : Math.floor(ms / 60000) % 60
+  let s = isNaN(ms) ? '--' : Math.floor(ms / 1000) % 60
+  return [h, m, s].map(v => v.toString().padStart(2, 0) ).join(':')
+}
