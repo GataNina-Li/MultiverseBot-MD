17769b1ea7be29368b4d0c3d54aa4d07fc1cb384
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 02:00:06 -0500

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -10,12 +10,29 @@ result = `「 *MEDIAFIRE DOWNLOAD* 」
 📊 Ukuran : ${res[0].size}
 💬 Link : ${res[0].link}
 _Tunggu Proses Upload Media_`
-m.reply(result)
-//await sleep(100)
-conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
-}
-handler.command = ['mediafire']
 
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message":  { 
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+} 
+  await m.reply(conn.sendBut(m.chat, result, `${wm}`, `✨ 𝗠𝗘𝗡𝗨`, '.menu', fkontak,  m ))
+ await m.reply(conn.sendBut(m.chat, global.wait, `${wm}`, `✨ 𝗠𝗘𝗡𝗨`, '.menu', fkontak,  m ))
+//m.reply(result)
+//await sleep(100)
+await conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true, mimetype:res[0].mime})
+}
+handler.help = ['mediafire'].map(v => v + ' *enlace*')
+handler.command = ['mediafire']
 module.exports = handler
 
 
