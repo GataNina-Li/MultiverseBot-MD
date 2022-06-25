acc47f97d8b17b625e79cbdd3f32dbb6d8317767
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 15:51:05 -0500

    Update downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -3,12 +3,29 @@ let xfar = require('xfarr-api')
 let handler = async (m, { usedPrefix, command, conn, args }) => {
 	 	  if (!args[0]) throw `Gunakan format: ${usedPrefix}${command} naruto`
 xfar.Pinterest(args[0]).then(async data => {
+	
+	const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+} 
+	
 let pincpt = `🔗Link media : ${data.url}`
-//conn.sendFile(m.chat,data.url, 'pin.jpg', pincpt,m)})
-conn.sendButtonImg(m.chat, Pinterest, pincpt, wm2, 'Thanks', 'thanks', m)
+conn.sendFile(m.chat,data.url, 'pin.jpg', pincpt, fkontak, m)})
+	
+//conn.sendButtonImg(m.chat, Pinterest, pincpt, wm2, 'Thanks', 'thanks', m)
 //conn.sendButtonImg(m.chat, gggd, mcng, wm2, 'Inventory', '.inv', m)
 	 //m.reply(conn.sendBut(m.chat, global.wait, `${wm}`, `✨ 𝗠𝗘𝗡𝗨`, '.menu', fkontak,  m ))
-}
+
 }
 			        
 
