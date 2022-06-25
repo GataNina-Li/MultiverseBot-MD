7cd1783ed1d8d31c825dd975ae31b1dbb072d3e9
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 17:58:50 -0500

    𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -20,7 +20,7 @@ xfar.Pinterest(args[0]).then(async data => {
 	
 let pincpt = `🔗Link media : ${data.url}`
 await conn.sendFile(m.chat, data.url, 'pin.jpg', `🌟 *Resultado de:* _${text}_\n${wm}`, fkontak, m)
- await conn.send3But(m.chat, wm, `ᴇɴʟᴀᴄᴇ\n${data.url}`, '𝗦𝗜𝗚𝗨𝗜𝗘𝗡𝗧𝗘  🌀', `${usedPrefix}pinterest ${text}`, '📡 𝗕𝗨𝗦𝗖𝗔𝗥 𝗘𝗡 𝗚𝗢𝗢𝗚𝗟𝗘', `${usedPrefix}image ${text}`, '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡', `.menu`, fkontak, m)})
+ await conn.send3But(m.chat, wm, `ᴇɴʟᴀᴄᴇ | ᴘɪɴᴛᴇʀᴇꜱᴛ \n${data.url}`, '𝗦𝗜𝗚𝗨𝗜𝗘𝗡𝗧𝗘  🌀', `${usedPrefix}pinterest ${text}`, '📡 𝗕𝗨𝗦𝗖𝗔𝗥 𝗘𝗡 𝗚𝗢𝗢𝗚𝗟𝗘', `${usedPrefix}image ${text}`, '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡', `.menu`, fkontak, m)})
  }
 handler.help = ['pinterest <keyword>']
 handler.tags = ['internet', 'downloader']
