c2555dc8edefdd8b5c6bb52f9e5f8f048457493f
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 01:29:06 -0500

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -20,7 +20,7 @@ module.exports = handler*/
 
 
 const fs = require('fs')
-const { mediafireDl } = require('@bochilteam/scraper')
+let { mediafireDl } = require('@bochilteam/scraper')
 //const { mediafiredl } from '@bochilteam/scraper'
 let handler = async (m, { conn, args, usedPrefix, command }) => {
 if (!args[0]) throw `${mg}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙑𝘼𝙇𝙄𝘿𝙊 𝘿𝙀 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀.\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙑𝘼𝙇𝙄𝘿 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀 𝙇𝙄𝙉𝙆.`
