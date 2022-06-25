107a58fe51e716bc8691a5cf882fcb6664c3058b
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 22:50:23 -0500

    Update downloader-igstory.js

--- plugins/downloader-igstory.js
+++ plugins/downloader-igstory.js
@@ -8,7 +8,24 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
   igstory(args[0]).then(async res => {
     let igs = JSON.stringify(res)
     let json = JSON.parse(igs)
-    await m.reply(global.wait)
+    
+    const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+} 
+    
+    await m.reply(global.wait, fkontak)
+    
     for (let { downloadUrl, type } of json)
       conn.sendFile(m.chat, downloadUrl, 'ig' + (type == 'image' ? '.jpg' : '.mp4'), '*Nih*', m)
 
