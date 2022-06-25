0cfc3e4c90835cd3d18e8ed1d8260fa762e549e7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 00:14:25 -0500

    Update downloader-mediafire.js

--- plugins/downloader-mediafire.js
+++ plugins/downloader-mediafire.js
@@ -1,4 +1,4 @@
-const { mediafireDl } = require('../lib/mediafire.js')
+/*const { mediafireDl } = require('../lib/mediafire.js')
 let handler = async (m, { conn, text, args, isPrems, isOwner, usedPrefix, command }) => {
 if (!text) return m.reply(`Kirim perintah ${usedPrefix + command} *link mediafire*`)
 if (!args[0].includes('mediafire.com')) return m.reply(error.linkmf)
@@ -16,4 +16,48 @@ conn.sendFile(m.chat, res[0].link, res[0].nama, null, m, false, {asDocument:true
 }
 handler.command = ['mediafire']
 
+module.exports = handler*/
+
+
+
+
+const fs = require('fs')
+//import { mediafiredl } from '@bochilteam/scraper'
+const { mediafiredl } = require('@bochilteam/scraper')
+let handler = async (m, { conn, args, usedPrefix, command }) => {
+if (!args[0]) throw `${mg}𝙄𝙉𝙂𝙍𝙀𝙎𝙀 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙑𝘼𝙇𝙄𝘿𝙊 𝘿𝙀 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀.\n\n𝙀𝙉𝙏𝙀𝙍 𝘼 𝙑𝘼𝙇𝙄𝘿 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀 𝙇𝙄𝙉𝙆.`
+try {
+let res = await mediafiredl(args[0])
+let { url, url2, filename, ext, aploud, filesize, filesizeH } = res
+let caption = `
+${eg}
+┃ 𓃠 *${gt} ${vs}*
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ 💫 𝙉𝙊𝙈𝘽𝙍𝙀 | 𝙉𝘼𝙈𝙀
+┃ ${filename}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ 💪 𝙋𝙀𝙎𝙊 |  𝙎𝙄𝙕𝙀
+┃ ${filesizeH}
+┃┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+┃ 🚀 𝙏𝙄𝙋𝙊 | 𝙏𝙔𝙋𝙀
+┃ ${ext}`.trim()
+conn.reply(m.chat, caption, m ) //{
+//contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, description: null, 
+//title: '𝙂𝙖𝙩𝙖𝘽𝙤𝙩-𝙈𝘿 | 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨',
+//body: 'Super Bot WhatsApp',         
+//previewType: 0, thumbnail: fs.readFileSync("./media/menus/Menu3.jpg"),
+//sourceUrl: `https://github.com/GataNina-Li/GataBot-MD`}}}
+  
+  
+conn.sendFile(m.chat, url, filename, '', m, null, { mimetype: ext, asDocument: true })
+} catch (e) { 
+m.reply(`${fg}𝙑𝙐𝙀𝙇𝙑𝘼 𝘼 𝙄𝙉𝙏𝙀𝙉𝙏𝘼𝙍. 𝘿𝙀𝘽𝙀 𝘿𝙀 𝙎𝙀𝙍 𝙐𝙉 𝙀𝙉𝙇𝘼𝘾𝙀 𝙑𝘼𝙇𝙄𝘿𝙊 𝘿𝙀 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀.
+𝙋𝙇𝙀𝘼𝙎𝙀 𝙏𝙍𝙔 𝘼𝙂𝘼𝙄𝙉. 𝙈𝙐𝙎𝙏 𝘽𝙀 𝘼 𝙑𝘼𝙇𝙄𝘿 𝙈𝙀𝘿𝙄𝘼𝙁𝙄𝙍𝙀 𝙇𝙄𝙉𝙆.`)
+console.log(e)
+}}
+handler.help = ['mediafire'].map(v => v + ' <url>')
+handler.tags = ['downloader']
+handler.command = /^(mediafire|mediafiredl|dlmediafire)$/i
+//handler.limit = 3
+//handler.exp = 100
 module.exports = handler
