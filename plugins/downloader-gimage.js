d55fa6b0ab2fa079f3013c6ef307f00ec157e225
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 18:32:03 -0500

    Update downloader-gimage.js

--- plugins/downloader-gimage.js
+++ plugins/downloader-gimage.js
@@ -22,10 +22,10 @@ const fkontak = {
   let results = await gis(text) || []
   let { url, width, height } = pickRandom(results) || {}
   if (!url) return m.reply('Not Found')
-  conn.sendButtonImg(m.chat, url, `
+  conn.send3ButtonImg(m.chat, url, `
 *── 「 GOOGLE IMAGE 」 ──*
 
-${text}`.trim(), `ɢᴏᴏɢʟᴇ ɪᴍᴀɢᴇɴ\nᴀɴᴄʜᴏ ⇢ ${width} | ᴀʟᴛᴜʀᴀ ⇢ ${height}\n${wm}`, '𝗦𝗜𝗚𝗨𝗜𝗘𝗡𝗧𝗘  🌀', `.gimage ${text}`, fkontak, m)
+${text}`.trim(), `ɢᴏᴏɢʟᴇ ɪᴍᴀɢᴇɴ\nᴀɴᴄʜᴏ ⇢ ${width} | ᴀʟᴛᴜʀᴀ ⇢ ${height}\n${wm}`, '𝗦𝗜𝗚𝗨𝗜𝗘𝗡𝗧𝗘  🌀', `.gimage ${text}`, '📡 𝗕𝗨𝗦𝗖𝗔𝗥 𝗘𝗡 𝗚𝗢𝗢𝗚𝗟𝗘', `${usedPrefix}image ${text}`, '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡', `.menu`, m)
 }
 handler.help = ['image <query>', 'gimage <query>', 'googleimage <query>']
 handler.tags = ['internet']
