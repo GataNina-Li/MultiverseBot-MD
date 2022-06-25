4016f6d2371248fee2de79b0ebf56bd7e9584829
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 15:38:22 -0500

    Update downloader-pinterest.js

--- plugins/downloader-pinterest.js
+++ plugins/downloader-pinterest.js
@@ -1,6 +1,3 @@
-const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
-let fs = require('fs')
-let fetch = require('node-fetch') 
 let xfar = require('xfarr-api')
 
 let handler = async (m, { usedPrefix, command, conn, args }) => {
@@ -8,7 +5,7 @@ let handler = async (m, { usedPrefix, command, conn, args }) => {
 xfar.Pinterest(args[0]).then(async data => {
 let pincpt = `🔗Link media : ${data.url}`
 //conn.sendFile(m.chat,data.url, 'pin.jpg', pincpt,m)})
-m.reply(conn.sendButtonImg(m.chat, data, pincpt, wm2, 'Thanks', 'thanks', m))
+conn.sendButtonImg(m.chat, xfar.Pinterest, pincpt, wm2, 'Thanks', 'thanks', m)
 //conn.sendButtonImg(m.chat, gggd, mcng, wm2, 'Inventory', '.inv', m)
 	 //m.reply(conn.sendBut(m.chat, global.wait, `${wm}`, `✨ 𝗠𝗘𝗡𝗨`, '.menu', fkontak,  m ))
 
