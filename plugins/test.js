199b6bf9b43f90c86ccaf241ae7a90270d5841ea
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-24 22:01:07 -0500

    Create test.js

---
+++ plugins/test.js
@@ -0,0 +1,35 @@
+// SI VAS A MODIFICAR AL MENOS NO QUITES LOS CREDITOS, WEB, ENLACES, DE ESA FORMA YA ESTAS APOYANDO 
+// PARA QUE SE PUEDA SEGUIR ELABORANDO EN ESTE LINDO MULTIVERSEBOT-MD 
+// TAMBIEN PUEDES AGREGAR TÚ INFORMACIÓN SIN ESTAR ELIMINANDO LOS CREDITOS 
+// EN CASO DE PODER COMPARTIR ESTE REPOSITORIO SE TE AGREDECE, Y GRACIAS POR INTERESARTE EN MULTIVERSEBOT-MD!! 
+let { MessageType } = require('@adiwajshing/baileys')
+let axios = require('axios')
+let fetch = require('node-fetch')
+let fs = require('fs')
+
+
+let handler = async (m, { conn, text } ) => {
+  let imgmenu = './src/github.jpg'
+  let miniurl = './src/github.jpg'
+  
+  conn.sendButton(m.chat, `*Test button*`, 'BY 4LB3RTO', imgmenu, [['Cʀᴇᴀᴅᴏʀ🐢', '.menu'], ['Iɴғᴏ📌', '.infobot']], false, { quoted: m, contextInfo: { externalAdReply: {
+  showAdAttribution: true,
+  mediaType: 'VIDEO',
+  mediaUrl: '',
+  title: '🐱⸽⃕NʏᴀɴCᴀᴛBᴏᴛ - MD🍁⃨፝⃕✰',
+  body: '4LB3RTO',
+  thumbnail: miniurl,
+  sourceUrl: 'https://chat.whatsapp.com/JdCIiS4NNmC4XyxWM70Ori'
+  }}})
+}
+   
+    
+
+handler.help = ['test']
+handler.tags = ['info']
+handler.command = ['test']
+
+module.exports = handler
+
+
+// MultiverseBot-MD
