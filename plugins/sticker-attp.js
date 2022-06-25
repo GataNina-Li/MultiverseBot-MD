ecdaea526ff1b4aeb3eefa3a04b2d565f31f8bee
ADOLFO SAN <104970935+ADOLFO-BOT5@users.noreply.github.com>
2022-06-25 00:27:51 -0500

    sticker-attp.js

--- plugins/sticker-attp.js
+++ plugins/sticker-attp.js
@@ -1,10 +1,11 @@
-let handler = async (m, { conn, text }) => {
-  let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
-  conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
+let handler = async (m, { conn, text, usedPrefix, command }) => {
+	if (!text) throw `🤖 _Envie el texto_\n\n🍃Ejemplo *${usedPrefix + command}* multiverse`
+    let teks = text ? text : m.quoted && m.quoted.text ? m.quoted.text : m.text
+    conn.sendFile(m.chat, global.API('xteam', '/attp', { file: '', text: teks }), 'attp.webp', '', m, false, { asSticker: true })
 }
-handler.help = ['attp <teks>']
+handler.help = ['attp <text>']
 handler.tags = ['sticker']
 
-handler.command = /^attp$/i
+handler.command = ['attp'] 
 
-module.exports = handler
+export default handler
