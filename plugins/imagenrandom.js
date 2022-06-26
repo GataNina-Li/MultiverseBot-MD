13e8e3f6f1b8f35d91d2da0c3cb57283e63ec20c
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-25 21:42:30 -0500

    Update imagenrandom.js

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -1,10 +1,23 @@
-const axios require('axios'); 
-let handler = async(m, { conn, usedPrefix, command }) => {
-let res = await axios("https://meme-api.herokuapp.com/gimme/sticker")
-let json = res.data
-let url = json.url
-conn.sendButton(m.chat, "*sticker*", author, url, [['⚽ SIGUIENTE ⚽', `${usedPrefix + command}`]], m)}
+const axios = require('axios')
+ let handler = async(m, { conn }) => {
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/sticker')
+            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}          
+𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫 `, m) 
+  }
 handler.help = ['imagenrandom']
-handler.tags = ['imagen']
-handler.command = /^(imagenrandom)$/i
+handler.tags = ['images']
+handler.command = /^(imagenrandom|random|epico)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
+
 module.exports = handler
