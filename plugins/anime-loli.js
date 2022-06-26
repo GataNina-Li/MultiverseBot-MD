c4a99a314ef4ae8c9aed0cb03f05553986239419
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-25 23:45:06 -0500

    Update anime-loli.js

--- plugins/anime-loli.js
+++ plugins/anime-loli.js
@@ -1,9 +1,24 @@
-let handler = async (m, { conn }) => {
-let api-hyzer = 'https://api.zacros.my.id/randomimg/loli'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.loli', m)
-}
-handler.help = ['loli']
-handler.tags = ['anime']
-handler.command = /^(loli)$/i
+const axios = require('axios')
+ let handler = async(m, { conn }) => {
+let les = await axios.get('https://meme-api.herokuapp.com/gimme/sticker')
+            conn.sendFile(m.chat, `${les.data.url}`, '', `${les.data.title}          
+🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m) 
+     conn.sendButtonImg(m.chat, les,'NEXT', '.imagenrandom', m)
+  }
+handler.help = ['imagenrandom']
+handler.tags = ['images']
+handler.command = /^(imagenrandom|random|epico)$/i
+handler.owner = false
+handler.mods = false
+handler.premium = false
+handler.group = false
+handler.private = false
+
+handler.admin = false
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 0
+handler.limit = false
 
 module.exports = handler
