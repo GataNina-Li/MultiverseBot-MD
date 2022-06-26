64757b1f28fe4a4cb1a6e159a0948d771424026a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-26 00:44:44 -0500

    Update imagen-random.js

--- plugins/imagen-random.js
+++ plugins/imagen-random.js
@@ -1,26 +1,10 @@
-
 let handler = async (m, { conn, text, usedPrefix, command }) => {	
 let api = 'https://meme-api.herokuapp.com/gimme/sticker'
-
-const fkontak = {
-	"key": {
-    "participants":"0@s.whatsapp.net",
-		"remoteJid": "status@broadcast",
-		"fromMe": false,
-		"id": "Halo"
-	},
-	"message": {
-		"contactMessage": {
-			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
-		}
-	},
-	"participant": "0@s.whatsapp.net"
-} 
 	
 //let pincpt = `🔗Link media : ${data.url}`
 //conn.sendButton(m.chat, gimme, wm, api, [['NEXT', `.random`]], m)
     //conn.sendButtonImg(m.chat, api, text, wm, m)
-	conn.sendButtonImg(m.chat, api, `Resultados de ${text}`, wm, 'Thanks', 'thanks', m) 
+	await conn.sendButtonImg(m.chat, api, `Resultados de ${text}`, wm, 'Thanks', '.thanks', m) 
     //conn.sendButtonLoc(m.chat, await(await fetch(api)).buffer(), 'Nih', wm2, 'NEXT', '.random', m)
 }
 handler.help = ['random']
