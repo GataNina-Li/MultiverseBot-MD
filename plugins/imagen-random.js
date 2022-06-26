208827793e42a8dd330a8467ef528b5b3a1f072a
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-25 23:55:29 -0500

    Update and rename imagenrandom.js to imagen-random.js

--- plugins/imagen-random.js
+++ plugins/imagen-random.js
@@ -1,6 +1,7 @@
 et handler = async (m, { conn }) => {
-let api-hyzer = 'https://meme-api.herokuapp.com/gimme/sticker'
-    conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.random', m)
+let api = 'https://meme-api.herokuapp.com/gimme/sticker'
+    conn.sendButtonImg(m.chat, api, 'Nih', wm2, 'NEXT', '.random', m)
+    
 }
 handler.help = ['random']
 handler.tags = ['imagen']
