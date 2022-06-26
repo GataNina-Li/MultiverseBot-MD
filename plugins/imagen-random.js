d5899257070f61728bab1c82f1b5e9d8c9596dc3
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-26 00:02:36 -0500

    Update imagen-random.js

--- plugins/imagen-random.js
+++ plugins/imagen-random.js
@@ -1,8 +1,8 @@
-let fetch = require('node-fetch')
+//let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 let api = 'https://meme-api.herokuapp.com/gimme/sticker'
-    //conn.sendButtonImg(m.chat, api, 'Nih', wm2, 'NEXT', '.random', m)
-    conn.sendButtonLoc(m.chat, await(await fetch(api)).buffer(), 'Nih', wm2, 'NEXT', '.random', m)
+    conn.send2ButtonImg(m.chat, api, '', wm, 'NEXT', '.random', 'NEXT', '.random', m)
+    //conn.sendButtonLoc(m.chat, await(await fetch(api)).buffer(), 'Nih', wm2, 'NEXT', '.random', m)
     
 }
 handler.help = ['random']
