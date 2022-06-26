eb7d80b117a35d9a3be9aa22768848eae26fa86a
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-25 19:39:45 -0500

    Update quotes-anime.js

--- plugins/quotes-anime.js
+++ plugins/quotes-anime.js
@@ -1,12 +1,26 @@
 let axios = require("axios");
 let handler = async(m, { conn, text }) => {
+
 	axios.get(`https://videfikri.com/api/anime/randomquoteanime`).then ((res) => {
 	 	let hasil = `${res.data.result.quotes}`
 
-	conn.reply(m.chat, hasil, m)
+    conn.reply(m.chat, hasil, m)
 	})
 }
 handler.help = ['quoteanime']
 handler.tags = ['anime']
 handler.command = /^(quoteanime)$/i
+handler.owner = true
+handler.mods = false
+handler.premium = false
+handler.group = true
+handler.private = false
+
+handler.admin = true
+handler.botAdmin = false
+
+handler.fail = null
+handler.exp = 100
+handler.limit = false
+
 module.exports = handler
