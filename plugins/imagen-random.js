9710188532a2084e0a8bccffce8036a12a5660c2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-26 00:17:52 -0500

    Update imagen-random.js

--- plugins/imagen-random.js
+++ plugins/imagen-random.js
@@ -1,9 +1,47 @@
-//let fetch = require('node-fetch')
+const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
+let fs = require('fs')
+let fetch = require('node-fetch') 
 let handler = async (m, { conn, text }) => {
 let api = 'https://meme-api.herokuapp.com/gimme/sticker'
 //conn.sendButton(m.chat, gimme, wm, api, [['NEXT', `.random`]], m)
-    conn.sendButtonImg(m.chat, api, text, wm, 'NEXT', '.random', m)
+    //conn.sendButtonImg(m.chat, api, text, wm, 'NEXT', '.random', m)
     //conn.sendButtonLoc(m.chat, await(await fetch(api)).buffer(), 'Nih', wm2, 'NEXT', '.random', m)
+let message = await prepareWAMessageMedia({ image: fs.readFileSync(api)}, { upload: conn.waUploadToServer })
+     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
+     templateMessage: {
+         hydratedTemplate: {
+           imageMessage: message.imageMessage,
+           hydratedContentText: text,
+           hydratedFooterText: `ᴍᴀʏᴏʀ ᴀ 250 ᴍɢ ᴛᴀʟ ᴠᴇᴢ ɴᴏ ꜱᴇ ᴇɴᴠɪᴇ\n${wm}`, 
+           hydratedButtons: [{
+             urlButton: {
+               displayText: '𝗚𝗜𝗧𝗛𝗨𝗕',
+               url: "https://github.com/GataNina-Li/MultiverseBot-MD"
+              }   
+            },
+              {
+              urlButton: {
+               displayText: '𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠',
+               url: instagram	           
+             }
+           },
+             {
+             quickReplyButton: {
+		     displayText: '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡',
+               id: '.menu',
+             }
+	   },
+             {
+             quickReplyButton: {
+               displayText: '𝗠𝗘𝗡𝗨 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗢 💫',
+               id: '.? all',
+             }
+          }]
+        }
+      }
+    }), { userJid: m.sender, quoted: m });
+    //conn.reply(m.chat, text.trim(), m)
+   await conn.relayMessage(m.chat, template.message, { messageId: template.key.id }) 
     
 }
 handler.help = ['random']
