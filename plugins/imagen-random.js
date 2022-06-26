f2b89c772e74565086873b90f2cf40e253cce872
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-26 00:34:24 -0500

    Update imagen-random.js

--- plugins/imagen-random.js
+++ plugins/imagen-random.js
@@ -1,5 +1,6 @@
 const fetch = require('node-fetch')
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+	try {
 let api = 'https://meme-api.herokuapp.com/gimme/sticker'
 let res = await fetch('https://meme-api.herokuapp.com/gimme/sticker')
 let img = await res.buffer()
@@ -21,12 +22,15 @@ const fkontak = {
 	
 //let pincpt = `🔗Link media : ${data.url}`
 conn.sendFile(m.chat, img, 'sticker.jpg', ``, fkontak, m)
+		
  conn.send2But(m.chat, wm, ``, '𝗦𝗜𝗚𝗨𝗜𝗘𝗡𝗧𝗘  🌀', `${usedPrefix + command}`, '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡', `.menu`, fkontak, m)})
- }
+} catch (e) {
+console.log(e)
+throw '*Error!*'
 //conn.sendButton(m.chat, gimme, wm, api, [['NEXT', `.random`]], m)
     //conn.sendButtonImg(m.chat, api, text, wm, 'NEXT', '.random', m)
     //conn.sendButtonLoc(m.chat, await(await fetch(api)).buffer(), 'Nih', wm2, 'NEXT', '.random', m)
-
+}
 handler.help = ['random']
 handler.tags = ['imagen']
 handler.command = /^(random)$/i
