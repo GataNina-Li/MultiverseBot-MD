d5f103d3175cedaaff3f191898751ccb009071c2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-26 00:03:32 -0500

    Update imagen-random.js

--- plugins/imagen-random.js
+++ plugins/imagen-random.js
@@ -1,7 +1,7 @@
 //let fetch = require('node-fetch')
 let handler = async (m, { conn }) => {
 let api = 'https://meme-api.herokuapp.com/gimme/sticker'
-    conn.send2ButtonImg(m.chat, api, '', wm, 'NEXT', '.random', 'NEXT', '.random', m)
+    conn.sendButtonImg(m.chat, api, '', wm, 'NEXT', '.random', m)
     //conn.sendButtonLoc(m.chat, await(await fetch(api)).buffer(), 'Nih', wm2, 'NEXT', '.random', m)
     
 }
