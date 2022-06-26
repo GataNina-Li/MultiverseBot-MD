0ed0988fd05c27544ed3a4418bae593000011a43
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-26 00:26:00 -0500

    Update imagen-random.js

--- plugins/imagen-random.js
+++ plugins/imagen-random.js
@@ -1,49 +1,32 @@
 const { default: makeWASocket, BufferJSON, WA_DEFAULT_EPHEMERAL, generateWAMessageFromContent, downloadContentFromMessage, downloadHistory, proto, getMessage, generateWAMessageContent, prepareWAMessageMedia } = require('@adiwajshing/baileys')
 let fs = require('fs')
 let fetch = require('node-fetch') 
-let handler = async (m, { conn, text }) => {
+let handler = async (m, { conn, text, usedPrefix, command }) => {
 let api = 'https://meme-api.herokuapp.com/gimme/sticker'
+
+const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message": {
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+} 
+	
+//let pincpt = `🔗Link media : ${data.url}`
+await conn.sendFile(m.chat, api, 'api.jpg', ``, fkontak, m)
+ await conn.send2But(m.chat, wm, ``, '𝗦𝗜𝗚𝗨𝗜𝗘𝗡𝗧𝗘  🌀', `${usedPrefix + command}`, '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡', `.menu`, fkontak, m)})
+ }
 //conn.sendButton(m.chat, gimme, wm, api, [['NEXT', `.random`]], m)
     //conn.sendButtonImg(m.chat, api, text, wm, 'NEXT', '.random', m)
     //conn.sendButtonLoc(m.chat, await(await fetch(api)).buffer(), 'Nih', wm2, 'NEXT', '.random', m)
-let message = await prepareWAMessageMedia({ image: fs.readFileSync(api)}, { upload: conn.waUploadToServer })
-     const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
-     templateMessage: {
-         hydratedTemplate: {
-           imageMessage: message.imageMessage,
-           hydratedContentText: text,
-           hydratedFooterText: `ᴍᴀʏᴏʀ ᴀ 250 ᴍɢ ᴛᴀʟ ᴠᴇᴢ ɴᴏ ꜱᴇ ᴇɴᴠɪᴇ\n${wm}`, 
-           hydratedButtons: [{
-             urlButton: {
-               displayText: '𝗚𝗜𝗧𝗛𝗨𝗕',
-               url: "https://github.com/GataNina-Li/MultiverseBot-MD"
-              }   
-            },
-              {
-              urlButton: {
-               displayText: '𝗜𝗡𝗦𝗧𝗔𝗚𝗥𝗔𝗠',
-               url: instagram	           
-             }
-           },
-             {
-             quickReplyButton: {
-		     displayText: '𝗠𝗘𝗡𝗨 𝗣𝗥𝗜𝗡𝗖𝗜𝗣𝗔𝗟 ⚡',
-               id: '.menu',
-             }
-	   },
-             {
-             quickReplyButton: {
-               displayText: '𝗠𝗘𝗡𝗨 𝗖𝗢𝗠𝗣𝗟𝗘𝗧𝗢 💫',
-               id: '.? all',
-             }
-          }]
-        }
-      }
-    }), { userJid: m.sender, quoted: m });
-    //conn.reply(m.chat, text.trim(), m)
-   await conn.relayMessage(m.chat, template.message, { messageId: template.key.id }) 
-    
-}
+
 handler.help = ['random']
 handler.tags = ['imagen']
 handler.command = /^(random)$/i
