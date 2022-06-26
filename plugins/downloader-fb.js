deb1e12f756f3bb551d2498efe0d4eb5cf5df589
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-25 19:29:15 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,16 +1,15 @@
-import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'
-let { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
+import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'let { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
 let handler = async (m, { conn, args }) => {
-    
-    if (!args[0]) throw `enlace?`
-    try {
-    const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-    for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*Url:* ${url}`, m)
-    } catch (e) {
-    await m.reply('error')
-    console.log(e)
-    }}
+
+if (!args[0]) throw `enlace?`
+try {
+const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
+for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*Url:* ${url}`, m)
+} catch (e) {
+await m.reply('error')
+console.log(e)
+}}
 handler.help = ['facebbok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^((facebook|fb)(downloder|dl)?)$/
+handler.command = /^((facebook|fb)(downloder|dl)?)$/i
 export default handler
