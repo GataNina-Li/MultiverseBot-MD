83e0dcda0c8d98e4a9d3d04c7256350ed699abc7
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-25 22:27:53 -0500

    Update imagenrandom.js

--- plugins/imagenrandom.js
+++ plugins/imagenrandom.js
@@ -2,7 +2,6 @@ const axios = require('axios')
  let handler = async(m, { conn }) => {
 let les = await axios.get('https://meme-api.herokuapp.com/gimme/sticker=${text}')
             conn.sendButtonImg(m.chat, les, `Resultados de ${text}`, siguiente, 'Thanks', 'thanks', m)
-𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫 `, m) 
   }
 handler.help = ['imagenrandom']
 handler.tags = ['images']
