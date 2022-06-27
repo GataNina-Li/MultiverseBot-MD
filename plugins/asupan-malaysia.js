a46f0e6a0de6c8cba1d1bd7be222ac8aa8b1014a
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-27 17:36:31 -0500

    `no envia nada`

--- plugins/asupan-malaysia.js
+++ plugins/asupan-malaysia.js
@@ -1,4 +1,6 @@
-let handler = async (m, { conn }) => {
+//no envia nada 
+
+/*let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zacros.my.id/asupan/malaysia'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.malaysia', m)
 }
@@ -6,4 +8,4 @@ handler.help = ['malaysia']
 handler.tags = ['asupan']
 handler.command = /^(malaysia)$/i
 
-module.exports = handler
+module.exports = handler\*
