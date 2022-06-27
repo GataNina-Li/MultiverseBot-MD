59b7a2f7786140765cd87f06ccf8c65f7b35488a
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-27 17:32:57 -0500

    `no envia nada `

--- plugins/asupan-korea.js
+++ plugins/asupan-korea.js
@@ -1,4 +1,6 @@
-let handler = async (m, { conn }) => {
+//no envia nada 
+
+/*let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zacros.my.id/asupan/korea'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.korea', m)
 }
@@ -6,5 +8,5 @@ handler.help = ['korea']
 handler.tags = ['asupan']
 handler.command = /^(korea)$/i
 
-module.exports = handler
+module.exports = handler\*
 
