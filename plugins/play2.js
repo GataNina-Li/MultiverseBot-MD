216f650751bfe2307be4107fd264176e4af46c78
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 14:03:21 -0500

    Update play2.js

--- plugins/play2.js
+++ plugins/play2.js
@@ -6,7 +6,8 @@ try {
   
 if (command == 'play.1') {
 conn.reply(m.chat, `Procesando...`, m)
-let res = await fetch("https://my-api-bice.vercel.app/api/ytplay?apikey=nktesla&q="+text)
+let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?"+text)
+//https://api.dhamzxploit.my.id/api/ytplaymp3?text=to%20the%20Bone%20lofi
 let json = await res.json()
 conn.sendFile(m.chat, json.descarga, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
   //conn.sendFile(m.chat, json.descarga, 'error.mp3', ``, m)
