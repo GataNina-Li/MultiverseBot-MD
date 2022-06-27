9d886041f1cc118b4d2a4ce8c777e97f443ccf09
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-27 18:50:30 -0500

    Update downloader-tiktok.js

--- plugins/downloader-tiktok.js
+++ plugins/downloader-tiktok.js
@@ -1,4 +1,4 @@
-const hxz = require("hxz-api")
+/*const hxz = require("hxz-api")
 let handler = async(m,{text, conn}) => {
 
 let p = await  hxz.ttdownloader(text)
@@ -9,4 +9,15 @@ handler.help = ['tiktok'].map(v => v + ' <url>')
 handler.tags = ['downloader']
 handler.command = /^(tiktok)$/i
 
+module.exports = handler/*
+
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn, args, usedPrefix, command, text }) => {
+if (!args[0]) throw `*Formato de uso: ${usedPrefix + command} https://tiktokxxxx*\n*Ejemplo:*\n*${usedPrefix + command} https://vm.tiktok.com/ZMLUWWsc3/*`
+if (!args[0].match(/tiktok/gi)) throw `*Fallo al detectar la URL de tiktok, compruebe que sea de tiktok*`
+let res = await fetch("https://api-alc.herokuapp.com/api/download/tiktok?url="+args[0]+"&apikey=ConfuMods")
+let json = await res.json()
+conn.sendFile(m.chat, json.result.sin_marca, 'error.mp4', `🤹‍♀️ *¡Aqui tienes el tiktok!*\n🐈 𝙂𝙖𝙩𝙖 𝘿𝙞𝙤𝙨 🐈`, m)}
+handler.command = /^(tik(tok)?(dl)?)$/i
 module.exports = handler
