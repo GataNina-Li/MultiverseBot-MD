0a0d9fe9f59458b420a91323d930190c0f9e28b2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 13:32:13 -0500

    Create play2.js

---
+++ plugins/play2.js
@@ -0,0 +1,26 @@
+let fs = require('fs')
+let fetch = require('node-fetch')
+let handler = async (m, {command, conn, text}) => {
+if (!text) throw `Nombre?`
+try {
+  
+if (command == 'play.1') {
+conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴀᴜᴅɪᴏ...⏳_*`, m)
+let res = await fetch("https://my-api-bice.vercel.app/api/ytplay?apikey=nktesla&q="+text)
+let json = await res.json()
+conn.sendFile(m.chat, json.descarga, 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
+  
+if (command == 'play.2') {
+conn.reply(m.chat, `*_⏳Sᴇ ᴇsᴛᴀ ᴘʀᴏᴄᴇsᴀɴᴅᴏ Sᴜ ᴠɪᴅᴇᴏ...⏳_*`, m)
+let res = await fetch("https://api.lolhuman.xyz/api/ytplay2?apikey=9b817532fadff8fc7cb86862&query="+text)
+let json = await res.json()
+conn.sendFile(m.chat, json.result.video, 'error.mp4', `_𝐓𝐡𝐞 𝐌𝐲𝐬𝐭𝐢𝐜 - 𝐁𝐨𝐭_`, m)} 
+  
+}catch(e){
+m.reply('*[❗𝐈𝐍𝐅𝐎❗] 𝙴𝚁𝚁𝙾𝚁, 𝙿𝙾𝚁 𝙵𝙰𝚅𝙾𝚁 𝚅𝚄𝙴𝙻𝚅𝙰 𝙰 𝙸𝙽𝚃𝙴𝙽𝚃𝙰𝚁𝙻𝙾*')
+console.log(e)
+}}
+handler.help = ['play.1' , 'play.2'].map(v => v + ' <texto>')
+handler.tags = ['downloader']
+handler.command = ['play.1', 'play.2']
+module.exports = handler 
