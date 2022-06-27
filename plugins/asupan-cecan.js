051e50af6ded35e0e01813fa51e6258a6529d6d7
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-27 17:17:46 -0500

    `no envia nada`

--- plugins/asupan-cecan.js
+++ plugins/asupan-cecan.js
@@ -1,4 +1,6 @@
-let handler = async (m, { conn }) => {
+//no envia nada 
+
+/*let handler = async (m, { conn }) => {
 let api-hyzer = 'https://api.zacros.my.id/asupan/cecan'
     conn.sendButtonImg(m.chat, api-hyzer, 'Nih', wm2, 'NEXT', '.cecan', m)
 }
@@ -6,4 +8,4 @@ handler.help = ['cecan']
 handler.tags = ['asupan']
 handler.command = /^(cecan)$/i
 
-module.exports = handler
+module.exports = handler\*
