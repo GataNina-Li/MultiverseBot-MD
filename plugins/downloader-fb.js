564aa6a6406762f04f784230c472ee5161bf4ab2
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 20:26:19 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,15 +1,16 @@
-import { facebookdl, facebookdlv2 } from '@bochilteam/scraper'let { facebookdl, facebookdlv2 } = require('@bochilteam/scraper')
-let handler = async (m, { conn, args }) => {
-
-if (!args[0]) throw `enlace?`
-try {
-const { result } = await facebookdl(args[0]).catch(async _ => await facebookdlv2(args[0]))
-for (const { url, isVideo } of result.reverse()) await conn.sendFile(m.chat, url, `facebook.${!isVideo ? 'bin' : 'mp4'}`, `*Url:* ${url}`, m)
-} catch (e) {
-await m.reply('error')
-console.log(e)
-}}
-handler.help = ['facebbok'].map(v => v + ' <url>')
+let fetch = require('node-fetch')
+const { fbdl } = require('../lib/fbdl')
+let handler = async(m, { conn, usedPrefix, args, command }) => {
+  if (!args[0]) throw `Masukkan URL Facebook yang ingin di download!\n\nContoh: ${usedPrefix + command} https://www.facebook.com/100009307660961/videos/2850837675236460/`
+  let v = await fbdl(args[0])
+  m.reply(wait)
+  await conn.sendFile(m.chat, v.hasil.link_high, 'fb.mp4', watermark, m)
+}
+handler.help = ['fb'].map(v => v + ' <url>')
 handler.tags = ['downloader']
-handler.command = /^((facebook|fb)(downloder|dl)?)$/i
-export default handler
+
+handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
+
+handler.limit = true
+
+module.exports = handler
