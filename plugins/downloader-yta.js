1367a828eadb96b608f36e5ec3ee2502949710a5
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-28 11:00:26 -0500

    Update downloader-yta.js

--- plugins/downloader-yta.js
+++ plugins/downloader-yta.js
@@ -2,30 +2,27 @@ let axios = require('axios')
 const fetch = require('node-fetch')
 let limit = 1024354
 const { servers, yta } = require('../lib/y2mate')
-let handler = async (m, { conn, args, isPrems, isOwner }) => {
-  if (!args || !args[0]) throw 'Uhm... urlnya mana?'
+let handler = async (m, { conn, args, isPrems, isOwner, usedPrefix, command }) => {
+  if (!args || !args[0]) throw `${mg}*Ingrese un enlace válido de YouTube para descargar el Audio*\n\n*EJEMPLO*\n*${usedPrefix + command}* https://www.youtube.com/watch?v=c5gJRzCi0f0&t=265s`
   let chat = global.db.data.chats[m.chat]
   let server = (args[1] || servers[0]).toLowerCase()
   let { dl_link, thumb, title, filesize, filesizeF} = await yta(args[0], servers.includes(server) ? server : servers[0])
   let isLimit = (isPrems || isOwner ? 99 : limit) * 1024 < filesize 
-  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', `
-┏┉━━━━━━━━━━━❏
-┆ *YOUTUBE MP3*
-├┈┈┈┈┈┈┈┈┈┈┈
-┆• *Judul:* ${title}
-│• *Type:* MP3
-┆• *📥 Ukuran File:* ${filesizeF}
-└❏
-`.trim(), m, null, {
-  asDocument: chat.useDocument
-})
+  if (!isLimit) conn.sendFile(m.chat, dl_link, title + '.mp3', 
+`*╭𝄗𝄗𝄗✦ *AUDIO* ✦𝄗𝄗𝄗⬣*
+ ⎸ *YOUTUBE MP3*
+ ⎸┈┈┈┈┈┈┈┈┈┈┈┈┈┈┈
+ ⎸🎧 *Título:* ${title}
+ ⎸🎧 *Tipo:* MP3
+ ⎸🎧 *Peso:* ${filesizeF}
+*╰𝄗𝄗✦ ⍟ ${vs} ✦𝄗𝄗⬣*`.trim(), m, null, { asDocument: chat.useDocument })
 }
-handler.help = ['mp3','a'].map(v => 'yt' + v + ` <url>`)
+handler.help = ['yta | ytmp3 *enlace*']
 handler.tags = ['downloader']
 handler.command = /^yt(a|mp3)$/i
 
 handler.fail = null
-handler.exp = 0
+handler.exp = 2
 handler.limit = true
 
 module.exports = handler
