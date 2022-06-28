b194fdee5600ed7d31e1922f10acb2fed630e434
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-28 00:07:48 -0500

    Update fotos_prueba.js

--- plugins/fotos_prueba.js
+++ plugins/fotos_prueba.js
@@ -1,5 +1,6 @@
 
 let handler = async (m, { conn }) => {
+     let handler = async (m, {usedPrefix, command, conn, text}) => {
 let video = fs.readFileSync(`./fotos/${pickRandom(['1','2','3','4','5','6','7','8','9','10','11','12','13','14','15','16','17','18','19','20','21','22','23','24','25','26','27','28','29','30','31','32','33','34','35','36','37','38','39','40','41'])}.jpg`)
 
      conn.sendFile(m.chat, video, '2.jpg', caption, m)
