75a016183822a513d84023ac3680eec4542b2e37
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 21:59:54 -0500

    𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫

--- plugins/downloader-play2.js
+++ plugins/downloader-play2.js
@@ -1,17 +1,17 @@
 let fs = require('fs')
 let fetch = require('node-fetch')
-let handler = async (m, {command, conn, text}) => {
-if (!text) throw `Nombre?`
+let handler = async (m, {usedPrefix, command, conn, text}) => {
+if (!text) throw `${mg}*Escriba el nombre o título para obtener el Vídeo*\n\n*EJEMPLO*\n*${usedPrefix + command}* Bellyache`
 try {
   
-if (command == 'play.1') {
+/*if (command == 'play.1') {
 conn.reply(m.chat, `Procesando...`, m)
 let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
 //https://api.dhamzxploit.my.id/api/ytplaymp3?text=to%20the%20Bone%20lofi
 let json = await res.json()
 conn.sendFile(m.chat, res.json(), 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
   //conn.sendFile(m.chat, json.descarga, 'error.mp3', ``, m)
- // conn.sendFile(m.chat, 'https://my-api-bice.vercel.app/api/ytplay?', 'apikey=nktesla&q="+text')
+ // conn.sendFile(m.chat, 'https://my-api-bice.vercel.app/api/ytplay?', 'apikey=nktesla&q="+text')*/
   
 if (command == 'play.2') {
 conn.reply(m.chat, `Procesando...`, m)
@@ -23,7 +23,7 @@ conn.sendFile(m.chat, json.result.video, 'error.mp4', wm, m)}
 m.reply('Error')
 console.log(e)
 }}
-handler.help = ['play.1 | play.2'].map(v => v + ' *texto*')
+handler.help = ['play.2'].map(v => v + ' *texto*')
 handler.tags = ['downloader']
-handler.command = ['play.1', 'play.2']
+handler.command = ['play.2']
 module.exports = handler 
