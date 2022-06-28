58b5ebb0f0225d81ac55d18dcee48eaa35569982
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-28 00:56:22 -0500

    Update memes.js

--- plugins/memes.js
+++ plugins/memes.js
@@ -11,5 +11,9 @@ function pickRandom(list) {
 }
 
 const asupan = [
-"https://i.pinimg.com/originals/2c/80/53/2c80538ba1cac99379dd70ef304f81d7.jpg",
+"https://i.pinimg.com/236x/9b/73/9e/9b739eca723bd817ad0a9d732555ebdb.jpg",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRECVxb-DhVxByWynyoA9Eitozj8vWTYhnBoxXKwDMKsosv0GKfw1_XG3haYVoGrHT3CsE&usqp=CAU",
+"https://i.pinimg.com/736x/6f/e8/b8/6fe8b8a9f603b889b59fa5c58fc0dde1.jpg",
+"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSoyKFwvl_OreufYQCazGDyVECdJtbYELhaqcVVuUFYCVWRfJ9fInYrwJIaAYjtGHjoVy0&usqp=CAU",
+"https://i.ytimg.com/vi/2aU89ku_FIk/maxresdefault.jpg"
 ]
