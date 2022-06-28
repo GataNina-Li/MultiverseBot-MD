bbd8941b08ea1c020f7a0bee2dd2aba46b289455
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-28 01:08:34 -0500

    `add memes videos `

--- plugins/memes_videos.js
+++ plugins/memes_videos.js
@@ -1,1 +1,17 @@
 
+let fs = require('fs')
+let fetch = require('node-fetch')
+let handler = async(m, { conn, usedPrefix, command }) => {  
+await conn.sendButtonVid(m.chat, pickRandom(asupan), '🔥🔥🔥🔥', 'Gata Dios', 'SIGUIENTE 🔄🥵', `${usedPrefix + command}`, m, false)
+}
+handler.command = /^(pornovid|pornovideo|Pornovid|Pornovideo|Pornvid|Ponrvid|pornvid|ponrvid|pornov)$/i
+module.exports = handler
+
+function pickRandom(list) {
+  return list[Math.floor(list.length * Math.random())]
+}
+
+const asupan = [
+"https://l.top4top.io/m_22572kvnt0.mp4",
+
+]
