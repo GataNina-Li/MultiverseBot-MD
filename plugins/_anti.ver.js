95a435b91deaf6124ba8fdc4b8b14962422a2724
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-28 12:10:13 -0500

    Create _anti.ver.js

---
+++ plugins/_anti.ver.js
@@ -0,0 +1,11 @@
+let handler = m => m
+handler.before = async function (m) {
+    if (!db.data.chats[m.chat].viewonce) return
+    let q = m.quoted ? m.quoted : m
+    if (q.mtype == 'viewOnceMessage') {
+    m.reply('Bot mendeteksi adanya media viewonce atau media sekali lihat, tunggu sebentar... bot akan meneruskan nya...')
+        await this.copyNForward(m.chat, await this.loadMessage(m.chat, q.id), false, { readViewOnce: true })
+    }
+}
+
+module.exports = handler
