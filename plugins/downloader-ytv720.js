5a846197e429c94a1d79a9881db8e9d942cd912e
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 21:50:36 -0500

    Update downloader-ytv720.js

--- plugins/downloader-ytv720.js
+++ plugins/downloader-ytv720.js
@@ -1,15 +1,14 @@
 //let xfar = require('xfarr-api')
+let fs = require('fs')
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text }) => {
     if (!text) throw 'Masukkan Link\n\nContoh: .ytv720 https://youtube.com/xxxxxx'
   //let res = await dzx.Youtube(text)
-    let dd = ("https://api.dhamzxploit.my.id/api/ytplaymp3?text=to%20the%20Bone%20lofi"+text)
-m.reply('*Tunggu Sebentar...*')
-conn.sendFile(m.chat, dd, '', `Youtube Downloader
-720p
-jika masih blur, berarti videonya emang ngeblur dari youtubenya
-Atau bisa jadi faktor *MUKA*`, m)
-
+conn.reply(m.chat, `Procesando...`, m)
+let res = await fetch("https://api.dhamzxploit.my.id/api/ytplaymp3?text="+text)
+//https://api.dhamzxploit.my.id/api/ytplaymp3?text=to%20the%20Bone%20lofi
+let json = await res.json()
+conn.sendFile(m.chat, res.json(), 'error.mp3', null, m, false, { mimetype: 'audio/mp4' })}
 }
 handler.help = ['ytv720 <url>']
 handler.tags = ['internet']
