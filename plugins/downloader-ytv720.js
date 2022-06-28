7709b61095b441fef6af39534fb0bf5a458c0277
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 21:18:40 -0500

    Update downloader-ytv720.js

--- plugins/downloader-ytv720.js
+++ plugins/downloader-ytv720.js
@@ -2,9 +2,10 @@
 let fetch = require('node-fetch')
 let handler = async (m, { conn, command, text }) => {
     if (!text) throw 'Masukkan Link\n\nContoh: .ytv720 https://youtube.com/xxxxxx'
-  let res = await dzx.Youtube(text)
+  //let res = await dzx.Youtube(text)
+    let dd = ("https://api.dhamzxploit.my.id/api/ytplaymp3?text=to%20the%20Bone%20lofi"+text)
 m.reply('*Tunggu Sebentar...*')
-conn.sendFile(m.chat, res.medias[2].url, '', `Youtube Downloader
+conn.sendFile(m.chat, dd, '', `Youtube Downloader
 720p
 jika masih blur, berarti videonya emang ngeblur dari youtubenya
 Atau bisa jadi faktor *MUKA*`, m)
