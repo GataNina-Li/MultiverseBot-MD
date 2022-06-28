1341f8e4d7d9bca118667d9554fb59d30623d658
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 19:16:16 -0500

    Update downloader-twitter.js

--- plugins/downloader-twitter.js
+++ plugins/downloader-twitter.js
@@ -3,13 +3,13 @@ let fs = require('fs')
 let fetch = require('node-fetch') 
 const { twitter } = require('../lib/scrape')
 let handler = async (m, { conn, args, usedPrefix, command }) => {
-  if (!args[0]) throw `*Perintah ini untuk mengunduh media twitter dengan link*\n\ncontoh:\n${usedPrefix + command} https://twitter.com/gofoodindonesia/status/1229369819511709697`
-  if (!args[0].match(/(https:\/\/.*twitter.com)/gi)) throw `*Link salah! Perintah ini untuk mengunduh media twitter dengan link*\n\ncontoh:\n${usedPrefix + command} https://twitter.com/gofoodindonesia/status/1229369819511709697`
+  if (!args[0]) throw `*Ingrese un enlace válido de Twitter para Obtener el Vídeo o GIF *\n\n*EJEMPLO*\n*${usedPrefix + command}* https://twitter.com/NetflixES/status/1541474691260354560?t=GVOvltFf9qCikmDOwW5lJw&s=19`
+  if (!args[0].match(/(https:\/\/.*twitter.com)/gi)) throw `*Debe de ser un enlace válido de Twitter*\n\n*EJEMPLO*\n*${usedPrefix + command}* https://twitter.com/NetflixES/status/1541474691260354560?t=GVOvltFf9qCikmDOwW5lJw&s=19`
 
   twitter(args[0]).then(async res => {
     let twit = JSON.stringify(res)
     let json = JSON.parse(twit)
-    let pesan = json.data.map((v) => `Enlace(s): ${v.url}`).join('\n------------\n')
+    let pesan = json.data.map((v) => `🌟 *Enlace:* ${v.url}`).join('\n*———————————————————*\n')
    // m.reply(pesan)
     let message = await prepareWAMessageMedia({ image: fs.readFileSync('./src/twitter.png')}, { upload: conn.waUploadToServer })
      const template = generateWAMessageFromContent(m.chat, proto.Message.fromObject({
@@ -17,7 +17,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
          hydratedTemplate: {
            imageMessage: message.imageMessage,
            hydratedContentText: pesan,
-           hydratedFooterText: `ᴘᴜᴇᴅᴇ ᴜꜱᴀʀ ᴇꜱᴛᴏꜱ ᴇɴʟᴀᴄᴇꜱ ᴘᴀʀᴀ ᴍᴏᴅɪꜰɪᴄᴀʀ ʟᴀ ᴄᴀʟɪᴅᴀᴅ\n${wm}`, 
+           hydratedFooterText: `ᴜꜱᴇ ᴇꜱᴛᴏꜱ ᴇɴʟᴀᴄᴇꜱ ᴘᴀʀᴀ ᴍᴏᴅɪꜰɪᴄᴀʀ ʟᴀ ᴄᴀʟɪᴅᴀᴅ\n${wm}`, 
            hydratedButtons: [{
              urlButton: {
                displayText: '𝗚𝗜𝗧𝗛𝗨𝗕',
@@ -49,7 +49,7 @@ let handler = async (m, { conn, args, usedPrefix, command }) => {
     conn.relayMessage(m.chat, template.message, { messageId: template.key.id })
     
     for (let { url } of json.data)
-      conn.sendFile(m.chat, url, 'ig' + (/mp4/i.test(url) ? '.mp4' : '.jpg'), watermark, m, false, { thumbnail: Buffer.alloc(0) })
+      conn.sendFile(m.chat, url, 'ig' + (/jpg/i.test(url) ? '.mp4' : '.jpg'), watermark, m, false, { thumbnail: Buffer.alloc(0) })
   })
 
 }
