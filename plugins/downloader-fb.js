f8427e21fc141d02d8b0df0e11c931c2fa7205d7
Gata Dios <96250885+GataNina-Li@users.noreply.github.com>
2022-06-27 20:44:02 -0500

    Update downloader-fb.js

--- plugins/downloader-fb.js
+++ plugins/downloader-fb.js
@@ -1,16 +1,29 @@
 let fetch = require('node-fetch')
 const { fbdl } = require('../lib/fbdl')
 let handler = async(m, { conn, usedPrefix, args, command }) => {
-  if (!args[0]) throw `Masukkan URL Facebook yang ingin di download!\n\nContoh: ${usedPrefix + command} https://www.facebook.com/100009307660961/videos/2850837675236460/`
+  if (!args[0]) throw `*Ingrese un enlace válido de Facebook para Obtener el Vídeo*\n\n*EJEMPLO*\n*${usedPrefix + command}* https://twitter.com/NetflixES/status/1541474691260354560?t=GVOvltFf9qCikmDOwW5lJw&s=19\n\n*${usedPrefix + command}* https://fb.watch/dW4TFRZyt-/`
   let v = await fbdl(args[0])
-  m.reply(wait)
+  //m.reply(wait)
+  const fkontak = {
+	"key": {
+    "participants":"0@s.whatsapp.net",
+		"remoteJid": "status@broadcast",
+		"fromMe": false,
+		"id": "Halo"
+	},
+	"message":  { 
+		"contactMessage": {
+			"vcard": `BEGIN:VCARD\nVERSION:3.0\nN:Sy;Bot;;;\nFN:y\nitem1.TEL;waid=${m.sender.split('@')[0]}:${m.sender.split('@')[0]}\nitem1.X-ABLabel:Ponsel\nEND:VCARD`
+		}
+	},
+	"participant": "0@s.whatsapp.net"
+}
+  m.reply(conn.sendBut(m.chat, global.wait, `${wm}`, `✨ 𝗠𝗘𝗡𝗨`, '.menu', fkontak,  m ))
   await conn.sendFile(m.chat, v.hasil.link_high, 'fb.mp4', watermark, m)
 }
-handler.help = ['fb'].map(v => v + ' <url>')
+handler.help = ['facebookvid | fbvid'].map(v => v + ' *enlace*')
 handler.tags = ['downloader']
 
-handler.command = /^f((b|acebook)(dl|download)?(er)?)$/i
-
+handler.command = /^facebookvid|fbvid$/i
 handler.limit = true
-
 module.exports = handler
