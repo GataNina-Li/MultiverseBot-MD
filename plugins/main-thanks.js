8dd1b3e15a47a3dea21a25117d0ebf37027b55fa
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-29 21:20:55 -0500

    𝑴𝒖𝒍𝒕𝒊𝒗𝒆𝒓𝒔𝒆𝑩𝒐𝒕-𝑴𝑫

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -1,4 +1,4 @@
-
+//arreglado by Erika
 let handler = async (m, { conn, text, usedPrefix, command }) => {
 
 conn.sendImageAsSticker(m.chat, pickRandom(stikerhuuu), m, { packname: "sticker by", author: "Erika" })
@@ -13,6 +13,6 @@ function pickRandom(list) {
   return list[Math.floor(list.length * Math.random())]
 }
 let stikerhuuu = [
- "https://telegra.ph/file/06a27a93b66f63a5a067e.jpg",//patrick huu
+ "https://telegra.ph/file/06a27a93b66f63a5a067e.jpg",//patrick gracias
 
 ]
