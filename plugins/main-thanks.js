d196a08a58fb725384209722248889bafc809418
ERIKA <106629294+Abiguelreyes75@users.noreply.github.com>
2022-06-29 21:08:35 -0500

    Update main-thanks.js

--- plugins/main-thanks.js
+++ plugins/main-thanks.js
@@ -1,20 +1,19 @@
 let handler = async (m, { conn, text, usedPrefix, command }) => {
+
 conn.sendImageAsSticker(m.chat, pickRandom(stikerrandom), m, { packname: "sticker by", author: "Elyas" })
-}  
-  
+}
+
 handler.customPrefix = /^(thanks)$/i
 handler.command = new RegExp
 
 module.exports = handler
 
 function pickRandom(list) {
-  return list[Math.floor(list.lenght * Math.random())]
-  
+  return list[Math.floor(list.length * Math.random())]
 }
-
 let stickerrandom = [
-'https://telegra.ph/file/20509b31b6e05ff999daa.jpg',
-'https://telegra.ph/file/06a27a93b66f63a5a067e.jpg' 
+"https://telegra.ph/file/20509b31b6e05ff999daa.jpg",
+"https://telegra.ph/file/06a27a93b66f63a5a067e.jpg"
 ]
 
 
